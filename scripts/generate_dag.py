fd26476b78bc013d7192a8e6dbd6278be991a0f4
Adler Santos <adlersantos@google.com>
2021-06-14 18:44:22 -0400

    feat: Support BigQueryToBigQueryOperator (#86)
    * feat: Support BQ to BQ operator
    
    * feat: Added BigQueryToBigQueryOperator in pipeline.yaml sample

--- scripts/generate_dag.py
+++ scripts/generate_dag.py
@@ -33,6 +33,7 @@ OPERATORS = {
     "GoogleCloudStorageToGoogleCloudStorageOperator",
     "GoogleCloudStorageDeleteOperator",
     "BigQueryOperator",
+    "BigQueryToBigQueryOperator",
     "KubernetesPodOperator",
 }
 
