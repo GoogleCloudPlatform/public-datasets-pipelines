95c303c0ab8f57a5293a5e9e24df66ba645d8679
Adler Santos <adlersantos@google.com>
2021-07-29 14:33:51 -0400

    revised container script to use one manifest file per date

--- datasets/geos_fp/_images/rolling_copy/script.py
+++ datasets/geos_fp/_images/rolling_copy/script.py
@@ -17,72 +17,84 @@ import logging
 import os
 import pathlib
 import subprocess
-import tempfile
 import typing
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 
 import bs4
 import requests
 from google.cloud import storage
 
+# The manifest file contains a list of files already downloaded for a given date
+MANIFEST_FILE = "manifest.txt"
+
 
 def main(
     base_url: str,
-    today: date,
+    dt: date,
     download_dir: pathlib.Path,
     target_bucket: str,
-    manifest_path: pathlib.Path,
-    days_rolling: int,
     batch_size: int,
 ) -> None:
-    # Collects all paths that span the last N days rolling.
-    # Example path: Y2021/M01/D01
-    source_paths = folder_paths_last_n_days(today, n_days=days_rolling)
-    create_local_dirs(download_dir, source_paths)
+    # Get date prefix, e.g. Y2021/M01/D01, and create directories for them
+    date_prefix = _date_prefix(dt)
+    (download_dir / date_prefix).mkdir(parents=True, exist_ok=True)
 
-    new_paths = set()
-    for source_path in source_paths:
-        url = f"{base_url}/{source_path}"
+    # Generate a set of all .nc4 files from the specified url and date
+    all_files = get_all_files(base_url, date_prefix)
 
-        response = requests.get(url)
-        if response.status_code == 200:
-            logging.info(f"Scraping .nc4 files in {url}")
-            webpage = bs4.BeautifulSoup(response.text, "html.parser")
-            new_paths.update(scrape(source_path, webpage))
-        else:
-            logging.warning(
-                f"The following URL doesn't exist, will try again later: {url}"
-            )
+    stored_files = get_stored_files(target_bucket, date_prefix, download_dir)
 
-    old_paths = get_old_paths(target_bucket, manifest_path)
-    for_download = urls_to_download(old_paths, new_paths)
+    # Files present in the source webpage but not yet stored on GCS
+    unstored_files = all_files - stored_files
 
-    copy_new_files(download_dir, for_download, manifest_path, batch_size, target_bucket)
-    remove_old_files_from_gcs(
-        download_dir, base_url, urls_to_delete(old_paths, new_paths)
+    download_and_store_new_files(
+        download_dir, date_prefix, unstored_files, batch_size, target_bucket
     )
-    update_manifest_file(new_paths, manifest_path, target_bucket)
 
 
-def folder_paths_last_n_days(today: date, n_days: int) -> typing.List[str]:
-    dates = [today - timedelta(days=n) for n in range(n_days)]
-
-    # Generates URLs to folders containing the .nc4 files, for example
+def _date_prefix(dt: date) -> typing.List[str]:
+    # Generates URL paths to folders containing the .nc4 files, for example
     # https://portal.nccs.nasa.gov/datashare/gmao/geos-fp/das/Y2021/M01/D01/
-    return [f"Y{dt.year}/M{dt.month:0>2}/D{dt.day:0>2}" for dt in dates]
+    # => Y2021/M01/D01
+    return f"Y{dt.year}/M{dt.month:0>2}/D{dt.day:0>2}"
 
 
-def create_local_dirs(
-    download_dir: pathlib.Path, source_paths: typing.List[str]
-) -> None:
-    for source_path in source_paths:
-        (download_dir / source_path).mkdir(parents=True, exist_ok=True)
+def get_all_files(base_url: str, date_prefix: str) -> typing.Set[str]:
+    all_files = set()
+    url = f"{base_url}/{date_prefix}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        logging.info(f"Scraping .nc4 files in {url}")
+        webpage = bs4.BeautifulSoup(response.text, "html.parser")
+        all_files.update(scrape(date_prefix, webpage))
+    else:
+        logging.warning(f"The following URL doesn't exist, will try again later: {url}")
+    return all_files
+
+
+def get_stored_files(
+    bucket_name: str, date_prefix: str, download_dir: pathlib.Path
+) -> typing.Set[str]:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    gcs_object = f"{date_prefix}/{MANIFEST_FILE}"
+    local_object = download_dir / MANIFEST_FILE
+
+    if storage.Blob(bucket=bucket, name=gcs_object).exists(storage_client):
+        logging.info(f"Manifest file found at gs://{bucket_name}/{gcs_object}")
+        blob = bucket.blob(gcs_object)
+        blob.download_to_filename(str(local_object))
+    else:
+        local_object.touch()
+
+    with open(local_object) as f:
+        return set(f.read().splitlines())
 
 
 def scrape(source_path: str, webpage: bs4.BeautifulSoup) -> typing.List[str]:
     file_paths = []
 
-    # Finds all URLs that end with ".nc4"
+    # Go through all the URLs in the page and collect the ones ending in ".nc4"
     for a_tag in webpage.find_all("a"):
 
         # The `href` property is the filename,
@@ -93,117 +105,83 @@ def scrape(source_path: str, webpage: bs4.BeautifulSoup) -> typing.List[str]:
     return file_paths
 
 
-def get_old_paths(bucket_name: str, manifest_path: pathlib.Path) -> typing.Set[str]:
-    name = "manifest.txt"
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-
-    if storage.Blob(bucket=bucket, name=name).exists(storage_client):
-        logging.info(f"Manifest file found at gs://{bucket_name}/{name}")
-        blob = bucket.blob(name)
-        blob.download_to_filename(str(manifest_path))
-        return set(manifest_path.read_text().split("\n"))
-    else:
-        manifest_path.touch()
-        return set()
-
-
-def download_item(parent_dir: pathlib.Path, file_path: str) -> None:
-    r = requests.get(f"{os.environ['BASE_URL']}/{file_path}", stream=True)
-    if r.status_code == 200:
-        with open(parent_dir / file_path, "wb") as f:
-            for chunk in r:
-                f.write(chunk)
-    else:
-        logging.warning(f"Couldn't download {file_path}: {r.text}")
-
-
-def copy_new_files(
+def download_and_store_new_files(
     download_dir: pathlib.Path,
-    file_paths: typing.List[str],
-    manifest_path: pathlib.Path,
+    date_prefix: str,
+    new_files: typing.Set[str],
     batch_size: int,
     target_bucket: str,
 ) -> None:
-    total_files = len(file_paths)
+    """In batches, download files from the source to the local filesystem
+    and upload them to the GCS target bucket
+    """
+    total_files = len(new_files)
     logging.info(f"Downloading {total_files} files.")
-    for n, batch in enumerate(batches(file_paths, batch_size=batch_size), 1):
+    for n, batch in enumerate(batches(list(new_files), batch_size=batch_size), 1):
         logging.info(
             f"Processing batch {n}: {(n - 1) * batch_size + 1} to {min(total_files, n * batch_size)}"
         )
-        copy_batch(batch, download_dir, target_bucket)
-
-        manifest = set(manifest_path.read_text().split("\n"))
-        manifest.update(batch)
-        update_manifest_file(
-            paths=manifest,
-            manifest_path=manifest_path,
-            target_bucket=target_bucket,
-        )
+        download_batch(batch, download_dir)
+        move_dir_contents_to_gcs(download_dir, target_bucket, date_prefix)
+        update_manifest_file(batch, download_dir, target_bucket, date_prefix)
 
 
-def copy_batch(
-    batch: typing.List[str],
-    download_dir: pathlib.Path,
-    target_bucket: str,
-) -> None:
+def download_batch(batch: typing.List[str], download_dir: pathlib.Path) -> None:
     for file_path in batch:
-        download_item(pathlib.Path(download_dir), file_path)
-    move_contents_to_gcs(pathlib.Path(download_dir), target_bucket)
-
-
-def move_contents_to_gcs(dir: pathlib.Path, target_bucket: str) -> None:
-    subprocess.check_call(
-        ["gsutil", "-m", "mv", "-r", f"{dir}/", f"gs://{target_bucket}"]
-    )
-
-
-def remove_old_files_from_gcs(
-    download_dir: pathlib.Path, base_url: str, file_paths: typing.List[str]
-) -> None:
-    logging.info(f"Deleting {len(file_paths)} files.")
-
-    if not file_paths:
-        return
-
-    # Save the urls in a temporary text file, so we can pass it into gsutil
-    # with the `-m` option to perform parallel deletions.
-    with tempfile.NamedTemporaryFile(dir=download_dir, mode="w") as tmp_file:
-        tmp_file.write(
-            "\n".join([f"{base_url}/{file_path}" for file_path in file_paths])
+        logging.info(f"Downloading file to {download_dir}/{file_path}")
+        subprocess.check_call(
+            [
+                "wget",
+                f"{os.environ['BASE_URL']}/{file_path}",
+                "-O",
+                f"{download_dir}/{file_path}",
+                "-nv",
+            ]
         )
 
-        ps = subprocess.Popen(["cat", tmp_file], stdout=subprocess.PIPE)
-        subprocess.check_output(["gsutil", "-m", "rm", "-I"], stdin=ps.stdout)
-        ps.wait()
+
+def move_dir_contents_to_gcs(
+    dir_: pathlib.Path, target_bucket: str, date_prefix: str
+) -> None:
+    subprocess.check_call(
+        [
+            "gsutil",
+            "-m",
+            "-o",
+            "GSUtil:parallel_composite_upload_threshold=250M",
+            "cp",
+            f"{dir_}/{date_prefix}/*.nc4",
+            f"gs://{target_bucket}/{date_prefix}",
+        ]
+    )
+    delete_dir_contents(dir_ / date_prefix)
+
+
+def delete_dir_contents(dir_to_delete: pathlib.Path) -> None:
+    """Delete directory contents, but not the dir itself. This is useful for keeping
+    date dirs such as Y2021/M07/D12 intact for the next batch of files to use.
+    """
+    [f.unlink() for f in dir_to_delete.glob("*") if f.is_file()]
 
 
 def update_manifest_file(
-    paths: typing.Set[str], manifest_path: pathlib.Path, target_bucket: str
+    paths: typing.Set[str],
+    download_dir: pathlib.Path,
+    target_bucket: str,
+    date_prefix: str,
 ) -> None:
-    with open(manifest_path, "w") as file_:
-        file_.write("\n".join(paths))
-    subprocess.check_call(["gsutil", "cp", manifest_path, f"gs://{target_bucket}"])
-
-
-def urls_to_download(
-    old_urls: typing.Set[str], new_urls: typing.Set[str]
-) -> typing.Set[str]:
-    urls = []
-    for new_url in new_urls:
-        if new_url not in old_urls:
-            urls.append(new_url)
-    return urls
-
-
-def urls_to_delete(
-    old_urls: typing.Set[str], new_urls: typing.Set[str]
-) -> typing.Set[str]:
-    urls = []
-    for old_url in old_urls:
-        if old_url not in new_urls:
-            urls.append(old_url)
-    return urls
+    manifest_path = download_dir / MANIFEST_FILE
+    with open(manifest_path, "a") as f:
+        f.write("\n".join(paths))
+        f.write("\n")
+    subprocess.check_call(
+        [
+            "gsutil",
+            "cp",
+            str(manifest_path),
+            f"gs://{target_bucket}/{date_prefix}/{MANIFEST_FILE}",
+        ]
+    )
 
 
 def batches(file_paths: typing.List[str], batch_size: int):
@@ -214,18 +192,15 @@ def batches(file_paths: typing.List[str], batch_size: int):
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
 
-    assert os.environ["TODAY"]
     assert os.environ["BASE_URL"]
+    assert os.environ["TODAY_DIFF"]
     assert os.environ["DOWNLOAD_DIR"]
     assert os.environ["TARGET_BUCKET"]
-    assert os.environ["MANIFEST_PATH"]
 
     main(
         base_url=os.environ["BASE_URL"],
-        today=datetime.strptime(os.environ["TODAY"], "%Y-%m-%d").date(),
+        dt=(date.today() - timedelta(days=int(os.environ["TODAY_DIFF"]))),
         download_dir=pathlib.Path(os.environ["DOWNLOAD_DIR"]).expanduser(),
-        manifest_path=pathlib.Path(os.environ["MANIFEST_PATH"]).expanduser(),
         target_bucket=os.environ["TARGET_BUCKET"],
-        days_rolling=int(os.getenv("DAYS_ROLLING", 10)),
-        batch_size=int(os.getenv("BATCH_SIZE", 100)),
+        batch_size=int(os.getenv("BATCH_SIZE", 10)),
     )
