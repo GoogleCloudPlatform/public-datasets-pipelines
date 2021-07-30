d95d41b1c883aa927e7372a542d75ab398c56728
Dipannita Banerjee <dipannitab@google.com>
2021-07-30 08:24:29 +0000

    feat: Onboard the chicago_crime dataset

---
+++ datasets/chicago_crime/_images/run_csv_transform_kub/csv_transform.py
@@ -0,0 +1,267 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# CSV transform for: chicago_crime.crime
+#
+#       Column Name                         Type            Length / Format                 Description
+#
+#       ID                                  integer         255                             ""
+#       Case Number                         string          255                             ""
+#       Date                                date          255                             ""
+#       Block                               string          512                             ""
+#       IUCR                                string          255                             ""
+#       Primary Type                        integer         255                             ""
+#       Description                         string          11/23/2020 01:41:21 PM          ""
+#       Location Description                integer         11/23/2020 01:41:21 PM          ""
+#       Arrest                              string          11/23/2020 01:41:21 PM          ""
+#       Domestic                            integer         11/23/2020 01:41:21 PM          ""
+#       Beat                                float           255                             ""
+#       District                            string          255                             ""
+#       Ward                                integer         255                             ""
+#       Community Area                      datetime        255                             ""
+#       FBI Code                            datetime        255                             ""
+#       X Coordinate                        datetime        255                             ""
+#       Y Coordinate                        datetime        255                             ""
+#       Year                                datetime        255                             ""
+#       Updated On                          datetime        255                             ""
+#       Latitude                            datetime        255                             ""
+#       Longitude                           datetime        255                             ""
+#       Location                            datetime        255                             ""
+
+
+import datetime
+import logging
+import math
+import os
+import pathlib
+
+# import numpy as np
+import requests
+import vaex
+from google.cloud import storage
+
+# import pdb
+
+
+# import typing
+
+
+def main(
+    source_url: str,
+    source_file: pathlib.Path,
+    target_file: pathlib.Path,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+):
+
+    logging.info(
+        "chicago crime process started at "
+        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    )
+
+    logging.info("creating 'files' folder")
+    pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
+
+    logging.info(f"Downloading file {source_url}")
+    download_file(source_url, source_file)
+
+    # open the input file
+    logging.info(f"Opening file {source_file}")
+    df = vaex.open(str(source_file))
+
+    # steps in the pipeline
+    logging.info(f"Transforming.. {source_file}")
+
+    logging.info(f"Transform: Rename columns.. {source_file}")
+    rename_headers(df)
+
+    logging.info(f"Transform: Converting date values.. {source_file}")
+    convert_values(df)
+
+    filter_null_rows(df)
+
+    logging.info("Transform: Reordering headers..")
+    df = df[
+        [
+            "unique_key",
+            "case_number",
+            "date",
+            "block",
+            "iucr",
+            "primary_type",
+            "description",
+            "location_description",
+            "arrest",
+            "domestic",
+            "beat",
+            "district",
+            "ward",
+            "community_area",
+            "fbi_code",
+            "x_coordinate",
+            "y_coordinate",
+            "year",
+            "updated_on",
+            "latitude",
+            "longitude",
+            "location"
+        ]
+    ]
+
+    # df.fillna("")
+
+    # convert data type format to integer
+    df["unique_key"] = df["unique_key"].apply(convert_to_int)
+    df["beat"] = df["beat"].apply(convert_to_int)
+    df["district"] = df["district"].apply(convert_to_int)
+    df["ward"] = df["ward"].apply(convert_to_int)
+    df["community_area"] = df["community_area"].apply(convert_to_int)
+    df["year"] = df["year"].apply(convert_to_int)
+
+    # convert data type format to float
+    df["x_coordinate"] = df["x_coordinate"].apply(resolve_nan)
+    df["y_coordinate"] = df["y_coordinate"].apply(resolve_nan)
+    df["latitude"] = df["latitude"].apply(resolve_nan)
+    df["longitude"] = df["longitude"].apply(resolve_nan)
+
+    # pdb.set_trace()
+
+    # save to output file
+    logging.info(f"Saving to output file.. {target_file}")
+    try:
+        save_to_new_file(df, file_path=str(target_file))
+    except Exception as e:
+        logging.error(f"Error saving output file: {e}.")
+
+    # upload to GCS
+    logging.info(
+        f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
+    )
+    upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
+
+    logging.info(
+        "Austin bikeshare stations process completed at "
+        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    )
+
+
+def resolve_nan(input: str) -> str:
+    str_val = ""
+    if input == "" or (math.isnan(input)):
+        str_val = ""
+    else:
+        str_val = str(input)
+    return str_val.replace("None", "")
+
+
+def convert_to_int(input: str) -> str:
+    str_val = ""
+    if input == "" or (math.isnan(input)):
+        str_val = ""
+    else:
+        str_val = str(int(round(input, 0)))
+    return str_val
+
+
+def rename_headers(df):
+    header_names = {
+        "ID": "unique_key",
+        "Case Number": "case_number",
+        "Date": "date",
+        "Block": "block",
+        "IUCR": "iucr",
+        "Primary Type": "primary_type",
+        "Description": "description",
+        "Location Description": "location_description",
+        "Arrest": "arrest",
+        "Domestic": "domestic",
+        "Beat": "beat",
+        "District": "district",
+        "Ward": "ward",
+        "Community Area": "community_area",
+        "FBI Code": "fbi_code",
+        "X Coordinate": "x_coordinate",
+        "Y Coordinate": "y_coordinate",
+        "Year": "year",
+        "Updated On": "updated_on",
+        "Latitude": "latitude",
+        "Longitude": "longitude",
+        "Location": "location"
+    }
+
+    for old_name, new_name in header_names.items():
+        df.rename(old_name, new_name)
+
+
+def convert_dt_format(dt_str):
+    # Old format: MM/dd/yyyy hh:mm:ss aa
+    # New format: yyyy-MM-dd HH:mm:ss
+    if dt_str is None or len(dt_str) == 0:
+        return dt_str
+    else:
+        return datetime.datetime.strptime(dt_str, "%m/%d/%Y %H:%M:%S %p").strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+
+def convert_values(df):
+    dt_cols = [
+        "date" , 
+        "updated_on"
+    ]
+
+    for dt_col in dt_cols:
+        df[dt_col] = df[dt_col].apply(convert_dt_format)
+
+    # int_cols = ["city_asset_number"]
+    # for int_col in int_cols:
+    #     df[int_col] = df[int_col].astype('Int64')
+
+
+def filter_null_rows(df):
+    df = df[df.unique_key != ""]
+
+
+def save_to_new_file(df, file_path):
+    df.export_csv(file_path)
+
+
+def download_file(source_url: str, source_file: pathlib.Path):
+    logging.info(f"Downloading {source_url} into {source_file}")
+    r = requests.get(source_url, stream=True)
+    if r.status_code == 200:
+        with open(source_file, "wb") as f:
+            for chunk in r:
+                f.write(chunk)
+    else:
+        logging.error(f"Couldn't download {source_url}: {r.text}")
+
+
+def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket)
+    blob = bucket.blob(gcs_path)
+    blob.upload_from_filename(file_path)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    main(
+        source_url=os.environ["SOURCE_URL"],
+        source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
+        target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
+        target_gcs_path=os.environ["TARGET_GCS_PATH"],
+    )
