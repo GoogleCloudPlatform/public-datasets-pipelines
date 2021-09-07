70449098364fbac91d84846caf599f0939a325e2
nlarge-google <nlarge@google.com>
2021-09-07 20:17:52 +0000

    feat: Adding GHCND By Year to NOAA dataset

---
+++ datasets/noaa/_images/run_csv_transform_kub_ghcnd_by_year/csv_transform.py
@@ -0,0 +1,167 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import logging
+import os
+import pathlib
+import gzip
+from ftplib import FTP
+
+import pandas as pd
+from google.cloud import storage
+
+
+def main(
+    source_url: str,
+    ftp_host: str,
+    ftp_dir: str,
+    ftp_filename: str,
+    source_file: pathlib.Path,
+    target_file: pathlib.Path,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+) -> None:
+
+    # source_url            STRING          -> The full url of the source file to transform
+    # ftp_host              STRING          -> The host IP of the ftp file (IP only)
+    # ftp_dir               STRING          -> The remote working directory that the FTP file resides in (directory only)
+    # ftp_filename          STRING          -> The name of the file to pull from the FTP site
+    # source_file           PATHLIB.PATH    -> The (local) path pertaining to the downloaded source file
+    # target_file           PATHLIB.PATH    -> The (local) target transformed file + filename
+    # target_gcs_bucket     STRING          -> The target GCS bucket to place the output (transformed) file
+    # target_gcs_path       STRING          -> The target GCS path ( within the GCS bucket ) to place the output (transformed) file
+
+    logging.info("NOAA - GHCND By Year process started")
+
+    logging.info("creating 'files' folder")
+    pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
+
+    source_file_zipped = str(source_file) + ".gz"
+    source_file_unzipped = str(source_file) + ".unzip"
+
+    logging.info(f"Downloading FTP file {source_url} from {ftp_host}")
+    download_file_ftp(ftp_host, ftp_dir, ftp_filename, source_file_zipped, source_url)
+
+    logging.info(f"Decompressing {source_file_unzipped}")
+    gz_decompress(source_file_zipped, source_file_unzipped)
+
+    logging.info(f"Adding header in {source_file}")
+    os.system(f"echo 'id,date,element,value,mflag,qflag,sflag,time' > {source_file}")
+    os.system(f"cat {source_file_unzipped} >> {source_file}")
+    os.unlink(source_file_unzipped)
+    os.unlink(source_file_zipped)
+
+    logging.info(f"Opening file {source_file}")
+    df = pd.read_csv(source_file)
+
+    logging.info(f"Transformation Process Starting.. {source_file}")
+
+    logging.info("Transform: Removing rows with blank id's..")
+    filter_null_rows(df)
+
+    logging.info("Transform: Converting Date Format..")
+    df["date"] = df["date"].apply(convert_dt_format)
+
+    logging.info(f"Transformation Process complete .. {source_file}")
+
+    logging.info(f"Saving to output file.. {target_file}")
+
+    try:
+        save_to_new_file(df, file_path=str(target_file))
+    except Exception as e:
+        logging.error(f"Error saving output file: {e}.")
+
+    logging.info(
+        f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
+    )
+    upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
+
+    logging.info("NOAA - GHCND By Year process completed")
+
+def gz_decompress(infile: str, tofile: str) -> None:
+    with open(infile, "rb") as inf, open(tofile, "w", encoding="utf8") as tof:
+        decom_str = gzip.decompress(inf.read()).decode("utf-8")
+        tof.write(decom_str)
+
+
+def filter_null_rows(df: pd.DataFrame) -> None:
+    df = df[df.id != ""]
+
+
+def convert_dt_format(dt_str: str) -> str:
+    if dt_str is None or len(str(dt_str)) == 0 or str(dt_str) == "nan":
+        return str(dt_str)
+    else:
+        return str(datetime.datetime.strptime(str(dt_str), "%Y%m%d").date().strftime("%Y-%m-%d"))
+
+
+def save_to_new_file(df, file_path) -> None:
+    df.to_csv(file_path, float_format="%.0f", index=False)
+
+
+def download_file_ftp(
+    ftp_host: str,
+    ftp_dir: str,
+    ftp_filename: str,
+    local_file: pathlib.Path,
+    source_url: str,
+) -> None:
+
+    # ftp_host      -> host ip (eg. 123.123.0.1)
+    # ftp_dir       -> working directory in ftp host where file is located
+    # ftp_filename  -> filename of FTP file to download
+    # source_file   -> local file (including path) to create containing ftp content
+
+    logging.info(f"Downloading {source_url} into {local_file}")
+    ftp_conn = FTP(ftp_host)
+    ftp_conn.login("", "")
+    ftp_conn.cwd(ftp_dir)
+    ftp_conn.encoding = 'utf-8'
+
+    try:
+        dest_file = open(local_file, "wb")
+        ftp_conn.encoding = "utf-8"
+        ftp_conn.retrbinary(
+            cmd="RETR " + ftp_filename,
+            callback=dest_file.write,
+            blocksize=1024,
+            rest=None,
+        )
+        ftp_conn.quit()
+        dest_file.close()
+    except Exception as e:
+        logging.error(f"Error saving output file: {e}.")
+
+
+def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket)
+    blob = bucket.blob(gcs_path)
+    blob.upload_from_filename(file_path)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    main(
+        source_url=os.environ["SOURCE_URL"],
+        ftp_host=os.environ["FTP_HOST"],
+        ftp_dir=os.environ["FTP_DIR"],
+        ftp_filename=os.environ["FTP_FILENAME"],
+        source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
+        target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
+        target_gcs_path=os.environ["TARGET_GCS_PATH"],
+    )
