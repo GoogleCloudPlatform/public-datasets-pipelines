e27fa4154b7a7ce5406b8794f90aea7da2968b66
Adler Santos <adlersantos@google.com>
2021-09-13 10:09:43 -0400

    chore: node pool affinities for `google_political_ads` pipelines

--- datasets/google_political_ads/last_updated/last_updated_dag.py
+++ datasets/google_political_ads/last_updated/last_updated_dag.py
@@ -38,6 +38,23 @@ with DAG(
         startup_timeout_seconds=600,
         name="last_updated",
         namespace="default",
+        affinity={
+            "nodeAffinity": {
+                "requiredDuringSchedulingIgnoredDuringExecution": {
+                    "nodeSelectorTerms": [
+                        {
+                            "matchExpressions": [
+                                {
+                                    "key": "cloud.google.com/gke-nodepool",
+                                    "operator": "In",
+                                    "values": ["pool-e2-standard-4"],
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        },
         image_pull_policy="Always",
         image="{{ var.json.google_political_ads.container_registry.run_csv_transform_kub }}",
         env_vars={
