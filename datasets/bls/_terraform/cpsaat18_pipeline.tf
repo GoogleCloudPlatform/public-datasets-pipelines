e9a58bea0d5bcec96ca92baa28559ca1fb1c5fc7
Vasu Chitte <vasuc@google.com>
2021-09-13 15:43:17 +0000

    chore: cleanup

---
+++ datasets/bls/_terraform/cpsaat18_pipeline.tf
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+resource "google_bigquery_table" "cpsaat18" {
+  project    = var.project_id
+  dataset_id = "bls"
+  table_id   = "cpsaat18"
+
+  description = "Current population survey 18: Employed persons by detailed industry, sex, race, and Hispanic or Latino ethnicity"
+
+  depends_on = [
+    google_bigquery_dataset.bls
+  ]
+}
+
+output "bigquery_table-cpsaat18-table_id" {
+  value = google_bigquery_table.cpsaat18.table_id
+}
+
+output "bigquery_table-cpsaat18-id" {
+  value = google_bigquery_table.cpsaat18.id
+}
