b0e2cde91ec64ab2cddf1a88cbf6843365a70cc2
Adler Santos <adlersantos@google.com>
2021-09-23 17:04:24 -0400

    fix: Delete temporary files generated by gsutil's parallel composite uploads

--- datasets/geos_fp/_images/rolling_copy/script.py
+++ datasets/geos_fp/_images/rolling_copy/script.py
@@ -154,6 +154,7 @@ def move_dir_contents_to_gcs(
             f"gs://{target_bucket}/{date_prefix}",
         ]
     )
+    delete_temp_pcu_objects(target_bucket)
     delete_dir_contents(dir_ / date_prefix)
 
 
@@ -162,6 +163,25 @@ def delete_dir_contents(dir_to_delete: pathlib.Path) -> None:
     date dirs such as Y2021/M07/D12 intact for the next batch of files to use.
     """
     [f.unlink() for f in dir_to_delete.glob("*") if f.is_file()]
+
+
+def delete_temp_pcu_objects(target_bucket: str) -> None:
+    """Delete temp GCS objects created by gsutil's parallel composite uploads.
+    See https://cloud.google.com/storage/docs/uploads-downloads#gsutil-pcu
+    """
+    res = subprocess.run(
+        ["gsutil", "ls", f"gs://{target_bucket}"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    uris = res.stdout.split()
+    for uri in uris:
+        object_name = uri.split(target_bucket + "/")[-1]
+        if not object_name.startswith("Y"):
+            subprocess.check_call(
+                ["gsutil", "rm", "-r", f"gs://{target_bucket}/{object_name}"],
+            )
 
 
 def update_manifest_file(
