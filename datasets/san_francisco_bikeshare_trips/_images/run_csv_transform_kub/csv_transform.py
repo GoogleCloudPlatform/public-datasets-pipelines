b0b9dfb99622b3cc455d1172f8b80d8e9b240cc2
nlarge-google <nlarge@google.com>
2021-10-12 04:04:43 +0000

    fix: next revision. not fully functional

--- datasets/san_francisco_bikeshare_trips/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/san_francisco_bikeshare_trips/_images/run_csv_transform_kub/csv_transform.py
@@ -18,7 +18,8 @@ import os
 import pathlib
 import tarfile as tf
 import zipfile as zip
-from subprocess import PIPE, Popen
+import shutil
+import fnmatch
 
 import numpy as pd
 import pandas as pd
@@ -28,7 +29,6 @@ from google.cloud import storage
 
 def main(
     source_url_http: str,
-    source_url_gs: str,
     source_file: pathlib.Path,
     target_file: pathlib.Path,
     chunksize: str,
@@ -39,14 +39,131 @@ def main(
     logging.info("San Francisco - Bikeshare Trips process started")
 
     pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
-
     dest_path = os.path.split(source_file)[0]
     download_url_files_from_list(source_url_http, dest_path)
-    download_url_files_from_list(source_url_gs, dest_path)
+    stage_input_files(dest_path, source_file)
+
+    trip_data_names = [
+        "trip_id",
+        "duration",
+        "start_date",
+        "start_station",
+        "start_terminal",
+        "end_date",
+        "end_station",
+        "end_terminal",
+        "bike_number",
+        "subscription_type",
+        "zip_code"
+    ]
+
+    tripdata_names = [
+        "duration_sec",
+        "start_time",
+        "end_time",
+        "start_station_id",
+        "start_station_name",
+        "start_station_latitude",
+        "start_station_longitude",
+        "end_station_id",
+        "end_station_name",
+        "end_station_latitude",
+        "end_station_longitude",
+        "bike_id",
+        "user_type",
+        "member_birth_year",
+        "member_gender",
+        "bike_share_for_all_trip"
+    ]
+
+    process_source_file(str(source_file).replace(".csv", "_trip_data.csv"), target_file, trip_data_names, int(chunksize))
+    process_source_file(str(source_file).replace(".csv", "_tripdata.csv"), target_file, tripdata_names, int(chunksize))
+
+
+def process_source_file(source_file: str, target_file: str, names: list, chunksize: int) -> None:
+    # logging.info(f"Opening batch file {source_file}")
+    with pd.read_csv(
+        source_file,                # path to main source file to load in batches
+        engine="python",
+        encoding="utf-8",
+        quotechar='"',              # string separator, typically double-quotes
+        chunksize=chunksize,        # size of batch data, in no. of records
+        sep=',',                    # data column separator, typically ","
+        header=None,                # use when the data file does not contain a header
+        names=names
+    ) as reader:
+        for chunk_number, chunk in enumerate(reader):
+            target_file_batch = str(target_file).replace(
+                ".csv", "-" + str(chunk_number) + ".csv"
+            )
+            df = pd.DataFrame()
+            df = pd.concat([df, chunk])
+            process_chunk(df, target_file_batch, target_file, (not chunk_number == 0))
+
+
+def process_chunk(
+    df: pd.DataFrame, target_file_batch: str, target_file: str, skip_header: bool
+) -> None:
+    logging.info(f"Processing file {target_file}")
+    if str(target_file).find("_trip_data.csv") > -1:
+        df = resolve_date_format(df)
+
+    if str(target_file).find("_tripdata.csv") > -1:
+        df = rename_headers(df)
+
+
+    save_to_new_file(df, file_path=str(target_file_batch))
+    append_batch_file(target_file_batch, target_file, skip_header, not(skip_header))
+
+
+def stage_input_files(dest_dir: str, target_file_path: str) -> None:
+    logging.info("Staging input files")
+    for src_dir in listdirs(dest_dir):
+        logging.info("-------------------------------------------------------------")
+        pool_files(src_dir, dest_dir, "tripdata.csv")
+        pool_files(src_dir, dest_dir, "trip_data.csv")
+    concatenate_files(target_file_path, dest_dir, "tripdata.csv")
+    concatenate_files(target_file_path, dest_dir, "trip_data.csv")
+
+
+def pool_files(src_dir: str, dest_dir: str, file_group_wildcard: str):
+    logging.info(f"Pooling files *{file_group_wildcard}")
+    if len(fnmatch.filter(os.listdir(src_dir), '*' + file_group_wildcard)) > 0:
+        for src_file in fnmatch.filter(os.listdir(src_dir), '*' + file_group_wildcard):
+            logging.info(f"copying {src_dir}/{src_file} -> {dest_dir}/{src_file}")
+            shutil.copyfile(f"{src_dir}/{src_file}", f"{dest_dir}/{src_file}")
+
+
+def concatenate_files(target_file_path: str, dest_path: str, file_group_wildcard: str) -> None:
+    target_file_dir = os.path.split(str(target_file_path))[0]
+    target_file_path = str(target_file_path).replace(".csv", "_" + file_group_wildcard)
+    logging.info(f"Concatenating files {target_file_dir}/*{file_group_wildcard}")
+    if(os.path.isfile(target_file_path)):
+        os.unlink(target_file_path)
+    for src_file_path in sorted(fnmatch.filter(os.listdir(dest_path), '*' + file_group_wildcard)):
+        src_file_path = dest_path + '/' + src_file_path
+        with open(src_file_path,'r') as src_file:
+            with open(target_file_path,'a+') as target_file:
+                src_file.readline # skip header line
+                logging.info(f"reading from file {src_file_path}, writing to file {target_file_path}")
+                for line in src_file:
+                    target_file.write(line)
+
+
+def listdirs(rootdir: str) -> list:
+    rtn_list = []
+    for file in os.listdir(rootdir):
+        d = os.path.join(rootdir, file)
+        if os.path.isdir(d):
+            rtn_list.append(d)
+            for elem in listdirs(d):
+                rtn_list.append(elem)
+    return rtn_list
 
 
 def download_url_files_from_list(url_list: str, dest_path: str):
-    for url in url_list.splitlines():
+    # for url in url_list.splitlines():
+    for url in url_list.split(","):
         url = url.replace('"', '').strip()
         dest_file = dest_path + '/' + os.path.split(url)[1]
         if url.find("http") == 0:
@@ -129,56 +246,20 @@ def download_url_files_from_list(url_list: str, dest_path: str):
     # logging.info(f"San Francisco - Bikeshare Trips process completed")
 
 
-def process_chunk(
-    df: pd.DataFrame, target_file_batch: str, target_file: str, skip_header: bool
-) -> None:
-    df = extract_columns(df)
-    df = rename_headers(df)
-    df = remove_empty_key_rows(df)
-    df = strip_whitespace(df)
-    df = remove_nan_cols(df)
-    df = resolve_date_format(df)
-    df = remove_parenthesis_long_lat(df)
-    df = generate_location(df)
-    df = reorder_headers(df)
-    save_to_new_file(df, file_path=str(target_file_batch))
-    append_batch_file(target_file_batch, target_file, skip_header, not(skip_header))
-
-
-def extract_columns(df_filedata: pd.DataFrame) -> pd.DataFrame:
-    # Example:
-    # ZZZ99999999  17.1167  -61.7833 TMAX 1949 1949
-    col_ranges = {
-        "id": slice(0, 11),             # LENGTH: 12  EXAMPLE ID:  123456789999
-        "latitude": slice(12, 20),      # LENGTH:  9  EXAMPLE LAT: -31.0123
-        "longitude": slice(21, 30),     # LENGTH: 10  EXAMPLE LON: -31.0123
-        "element": slice(31, 35),       # LENGTH:  5  EXAMPLE ELE: ZZZZ
-        "firstyear": slice(36, 40),     # LENGTH:  5  EXAMPLE FIRSTYEAR: 1901
-        "lastyear": slice(41, 45),      # LENGTH:  5  EXAMPLE LASTYEAR: 1902
-    }
-
-    df = pd.DataFrame()
-
-    def get_column(col_val: str, col_name: str) -> str:
-        return col_val.strip()[col_ranges[col_name]].strip()
-
-    for col_name in col_ranges.keys():
-        df[col_name] = df_filedata["textdata"].apply(get_column, args=(col_name,))
-
-    # remove the incidental header
-    df = df[df["id"] != "textdata"]
-
-    return df
-
-
-def rename_headers(df: pd.DataFrame) -> pd.DataFrame:
-    header_names = {
-        "Field 1": "New Name Field 1",
-        "Field 2": "New Name Field 2",
-    }
-    df = df.rename(columns=header_names)
-
-    return df
+# def process_chunk(
+#     df: pd.DataFrame, target_file_batch: str, target_file: str, skip_header: bool
+# ) -> None:
+#     df = extract_columns(df)
+#     df = rename_headers(df)
+#     df = remove_empty_key_rows(df)
+#     df = strip_whitespace(df)
+#     df = remove_nan_cols(df)
+#     df = resolve_date_format(df)
+#     df = remove_parenthesis_long_lat(df)
+#     df = generate_location(df)
+#     df = reorder_headers(df)
+#     save_to_new_file(df, file_path=str(target_file_batch))
+#     append_batch_file(target_file_batch, target_file, skip_header, not(skip_header))
 
 
 def remove_empty_key_rows(df: pd.DataFrame) -> pd.DataFrame:
@@ -199,38 +280,11 @@ def strip_whitespace(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def remove_nan(dt_str: str) -> int:
-    if not dt_str or str(dt_str) == "nan":
-        return int()
-    else:
-        return int(dt_str)
-
-
-def remove_nan_cols(df: pd.DataFrame) -> pd.DataFrame:
-    logging.info("Resolving NaN data")
-    cols = {
-        "count_qualified",
-        "existing_installs_count",
-        "number_of_panels_n",
-        "number_of_panels_s",
-        "number_of_panels_e",
-        "number_of_panels_w",
-        "number_of_panels_f",
-        "number_of_panels_median",
-        "number_of_panels_total",
-    }
-
-    for col in cols:
-        df[col] = df[col].apply(remove_nan)
-
-    return df
-
-
 def resolve_date_format(df: pd.DataFrame) -> pd.DataFrame:
     logging.info("Resolving Date Format")
     date_fields = [
-        "datefield1",
-        "datefield2",
+        "start_date",
+        "end_date",
     ]
 
     for dt_fld in date_fields:
@@ -244,7 +298,7 @@ def convert_dt_format(dt_str: str) -> str:
     elif (
         dt_str.strip()[2] == "/"
     ):  # if there is a '/' in 3rd position, then we have a date format mm/dd/yyyy
-        return datetime.datetime.strptime(dt_str, "%m/%d/%Y %H:%M:%S %p").strftime(
+        return datetime.datetime.strptime(dt_str, "%m/%d/%Y %H:%M").strftime(
             "%Y-%m-%d %H:%M:%S"
         )
     else:
@@ -367,7 +421,6 @@ if __name__ == "__main__":
 
     main(
         source_url_http=os.environ["SOURCE_URL_HTTP"],
-        source_url_gs=os.environ["SOURCE_URL_GS"],
         source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
         target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
         chunksize=os.environ["CHUNKSIZE"],
