c2fb0257d4acaa7d0c1995a3544a7aa3bd008c2d
nlarge-google <nlarge@google.com>
2021-10-29 16:01:28 +0000

    fix: Regenerated some dags.

--- datasets/epa_historical_air_quality/annual_summaries/annual_summaries_dag.py
+++ datasets/epa_historical_air_quality/annual_summaries/annual_summaries_dag.py
@@ -13,10 +13,9 @@
 # limitations under the License.
 
 
-from airflow.providers.cncf.kubernetes.operators import kubernetes_pod
 from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators import kubernetes_pod
 from airflow.providers.google.cloud.transfers import gcs_to_bigquery
-
 
 default_args = {
     "owner": "Google",
