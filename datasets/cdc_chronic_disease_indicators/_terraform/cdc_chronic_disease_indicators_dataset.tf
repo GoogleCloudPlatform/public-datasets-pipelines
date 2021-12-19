e3e7dedda9b4f3441f1d3d680ba53f35e98ff117
Varshika Gupta <varshika@google.com>
2021-12-19 16:24:58 +0000

    feat: Onboard Chronic Disease Indicators Dataset

--- datasets/cdc_chronic_disease_indicators/_terraform/cdc_chronic_disease_indicators_dataset.tf
+++ datasets/cdc_chronic_disease_indicators/_terraform/cdc_chronic_disease_indicators_dataset.tf
@@ -24,3 +24,14 @@ resource "google_bigquery_dataset" "cdc_chronic_disease_indicators" {
 output "bigquery_dataset-cdc_chronic_disease_indicators-dataset_id" {
   value = google_bigquery_dataset.cdc_chronic_disease_indicators.dataset_id
 }
+
+resource "google_storage_bucket" "cdc-chronic-disease-indicators" {
+  name                        = "${var.bucket_name_prefix}-cdc-chronic-disease-indicators"
+  force_destroy               = true
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+output "storage_bucket-cdc-chronic-disease-indicators-name" {
+  value = google_storage_bucket.cdc-chronic-disease-indicators.name
+}
