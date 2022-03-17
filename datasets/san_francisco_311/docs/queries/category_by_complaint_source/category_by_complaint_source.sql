854361768ae889429e311505307189619cfe4c6b
Michelle Lohr <98989192+chellebodnar-google@users.noreply.github.com>
2022-03-17 15:14:26 -0700

    Add better comments
    Add comments and change variable "num_instances" to "num_compaints" for clarity

--- datasets/san_francisco_311/docs/queries/category_by_complaint_source/category_by_complaint_source.sql
+++ datasets/san_francisco_311/docs/queries/category_by_complaint_source/category_by_complaint_source.sql
@@ -1,10 +1,10 @@
-# What is the most likely category corresponding to each complaint source?
+# What is the most common category for each complaint source?
 
 WITH source_category_counts AS (
   SELECT
     source,
     category,
-    COUNT(1) AS num_instances
+    COUNT(1) AS num_complaints
   FROM
     `bigquery-public-data`.san_francisco_311.311_service_requests 
   GROUP BY
@@ -13,15 +13,18 @@ WITH source_category_counts AS (
 SELECT 
   source,
   category, 
-  num_instances, 
-  num_instances/total AS fraction_of_source
+  num_complaints, 
+  num_complaints/total AS fraction_of_source
 FROM
   (SELECT 
      source,
      category, 
-     num_instances,
-     ROW_NUMBER() OVER (PARTITION BY source ORDER BY num_instances DESC) AS rank,
-     SUM(num_instances) OVER (PARTITION BY source) total
+     num_complaints,
+     # Within each source, rank the categories by number of complaints in descending order. 
+     ROW_NUMBER() OVER (PARTITION BY source ORDER BY num_complaints DESC) AS rank,
+     # Compute the total number of complaints reported per source
+     SUM(num_complaints) OVER (PARTITION BY source) total
    FROM source_category_counts)
  WHERE
+   # Extract the most common category of complaint
    rank = 1;
