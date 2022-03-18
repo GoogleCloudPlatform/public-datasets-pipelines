ea2213cb8a6b27d555ed1c836a487c57300b2822
Arjun Gill <arjungill@google.com>
2022-03-18 16:48:32 +0000

    added function to retrieve gcs bucket and updated readme

--- datasets/google_dei/infra/diversity_annual_report_pipeline.tf
+++ datasets/google_dei/infra/diversity_annual_report_pipeline.tf
@@ -16,10 +16,15 @@
 
 
 resource "google_bigquery_table" "google_dei_dar_intersectional_attrition" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_intersectional_attrition"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_intersectional_attrition"
+
   description = "This table contains the attrition index score of Googlers in the U.S. cut by race and gender combined. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -34,10 +39,15 @@ output "bigquery_table-google_dei_dar_intersectional_attrition-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_intersectional_hiring" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_intersectional_hiring"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_intersectional_hiring"
+
   description = "This table contains the hiring breakdown of Googlers in the U.S. cut by race and gender combined. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -52,10 +62,15 @@ output "bigquery_table-google_dei_dar_intersectional_hiring-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_intersectional_representation" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_intersectional_representation"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_intersectional_representation"
+
   description = "This table contains the representation of Googlers in the U.S. cut by race and gender combined. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -70,10 +85,15 @@ output "bigquery_table-google_dei_dar_intersectional_representation-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_non_intersectional_representation" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_non_intersectional_representation"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_non_intersectional_representation"
+
   description = "This table contains the representation of Googlers in the U.S. cut by race and gender separately and the representation of global Googlers cut by gender. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -88,10 +108,15 @@ output "bigquery_table-google_dei_dar_non_intersectional_representation-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_non_intersectional_attrition" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_non_intersectional_attrition"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_non_intersectional_attrition"
+
   description = "This table contains the attrition index score of Googlers in the U.S. cut by race and gender separately and the attrition index score of global Googlers cut by gender. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -106,10 +131,15 @@ output "bigquery_table-google_dei_dar_non_intersectional_attrition-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_non_intersectional_hiring" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_non_intersectional_hiring"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_non_intersectional_hiring"
+
   description = "This table contains the hiring breakdown of Googlers in the U.S. cut by race and gender separately and the hiring breakdown of global Googlers cut by gender. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -124,10 +154,15 @@ output "bigquery_table-google_dei_dar_non_intersectional_hiring-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_region_non_intersectional_attrition" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_region_non_intersectional_attrition"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_region_non_intersectional_attrition"
+
   description = "This table contains the attrition index score of Googlers in the regions (EMEA, APAC, and the Americas) cut by gender. \"Americas\" includes all countries in North and South America in which we operate, excluding the U.S. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -142,10 +177,15 @@ output "bigquery_table-google_dei_dar_region_non_intersectional_attrition-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_region_non_intersectional_hiring" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_region_non_intersectional_hiring"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_region_non_intersectional_hiring"
+
   description = "This table contains the hiring breakdown of Googlers in the regions (EMEA, APAC, and the Americas) cut by gender. \"Americas\" includes all countries in North and South America in which we operate, excluding the U.S. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -160,10 +200,15 @@ output "bigquery_table-google_dei_dar_region_non_intersectional_hiring-id" {
 }
 
 resource "google_bigquery_table" "google_dei_dar_region_non_intersectional_representation" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_region_non_intersectional_representation"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_region_non_intersectional_representation"
+
   description = "This table contains the representation of Googlers in the regions (EMEA, APAC, and the Americas) cut by race and gender. \"Americas\" includes all countries in North and South America in which we operate, excluding the U.S. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
@@ -178,10 +223,15 @@ output "bigquery_table-google_dei_dar_region_non_intersectional_representation-i
 }
 
 resource "google_bigquery_table" "google_dei_dar_self_id" {
-  project     = var.project_id
-  dataset_id  = "google_dei"
-  table_id    = "dar_self_id"
+  project    = var.project_id
+  dataset_id = "google_dei"
+  table_id   = "dar_self_id"
+
   description = "This table contains the representation of Googlers globally who identify as LGBTQ+, members of the military or veterans, people with disabilities, or non-binary genders. Some data may be intentionally redacted due to security and privacy restrictions regarding smaller n-counts. In those cases, the data is displayed as a null value."
+
+
+
+
   depends_on = [
     google_bigquery_dataset.google_dei
   ]
