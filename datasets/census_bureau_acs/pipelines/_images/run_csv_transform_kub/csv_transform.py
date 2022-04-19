b97e894b5c925cb316f3eaf06d94f6946064f946
Naveen Noel <nnoel@google.com>
2022-04-19 17:04:32 +0000

    fix: integrating to one pipeline.

--- datasets/census_bureau_acs/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/census_bureau_acs/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 
-import datetime
 import json
 import logging
 import os
@@ -23,38 +22,79 @@ import typing
 import numpy as np
 import pandas as pd
 import requests
-from google.cloud import storage
+from google.api_core.exceptions import NotFound
+from google.cloud import storage, bigquery
 
 
 def main(
     source_url: str,
-    year_report: str,
-    api_naming_convention: str,
     target_file: pathlib.Path,
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_path: str,
+    pipeline_name: str,
     target_gcs_bucket: str,
     target_gcs_path: str,
-    headers: typing.List[str],
-    rename_mappings: dict,
-    pipeline_name: str,
+    year_report: str,
+    api_naming_convention: str,
     geography: str,
     report_level: str,
-    concat_col: typing.List[str],
+    concat_col_list: typing.List[str],
+    group_id_file: str,
+    state_code_file: str,
+    rename_mappings_list: dict,
+    input_csv_headers: typing.List[str],
 ) -> None:
 
-    logging.info(
-        f"ACS {pipeline_name} process started at "
-        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    )
-
+    logging.info(f"{pipeline_name} process started")
     logging.info("Creating 'files' folder")
     pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
+    execute_pipeline(
+        group_id_file="group_ids.json",
+        state_code_file="state_codes.json",
+        report_level=report_level,
+        year_report=year_report,
+        api_naming_convention=api_naming_convention,
+        source_url=source_url,
+        rename_mappings_list=rename_mappings_list,
+        geography=geography,
+        concat_col_list=concat_col_list,
+        input_csv_headers=input_csv_headers,
+        target_file=target_file,
+        target_gcs_bucket=target_gcs_bucket,
+        target_gcs_path=target_gcs_path,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        destination_table=table_id,
+        schema_path=schema_path
+    )
+    logging.info(f"{pipeline_name} process completed")
 
-    json_obj_group_id = open("group_ids.json")
+
+def execute_pipeline(
+    group_id_file: str,
+    state_code_file: str,
+    report_level: str,
+    year_report: str, 
+    api_naming_convention: str, 
+    source_url: str,
+    rename_mappings_list: dict,
+    geography: str,
+    concat_col_list: typing.List[str],
+    input_csv_headers: typing.List[str],
+    target_file: str,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+    project_id: str,
+    dataset_id: str,
+    destination_table: str,
+    schema_path: str
+) -> None:
+    json_obj_group_id = open(group_id_file)
     group_id = json.load(json_obj_group_id)
-
-    json_obj_state_code = open("state_codes.json")
+    json_obj_state_code = open(state_code_file)
     state_code = json.load(json_obj_state_code)
-
     logging.info("Extracting the data from API and loading into dataframe...")
     if report_level == "national_level":
         df = extract_data_and_convert_to_df_national_level(
@@ -64,45 +104,174 @@ def main(
         df = extract_data_and_convert_to_df_state_level(
             group_id, state_code, year_report, api_naming_convention, source_url
         )
-
     logging.info("Replacing values...")
     df = df.replace(to_replace={"KPI_Name": group_id})
-
-    logging.info("Renaming headers...")
-    rename_headers(df, rename_mappings)
-
-    logging.info("Creating column geo_id...")
+    rename_headers(df, rename_mappings_list)
     if geography == "censustract" or geography == "blockgroup":
         df["tract"] = df["tract"].apply(pad_zeroes_to_the_left, args=(6,))
         df["state"] = df["state"].apply(pad_zeroes_to_the_left, args=(2,))
         df["county"] = df["county"].apply(pad_zeroes_to_the_left, args=(3,))
+    df = create_geo_id(df, concat_col_list)
+    df = pivot_dataframe(df)
+    logging.info("Reordering headers...")
+    df = df[input_csv_headers]
+    logging.info(f"Saving to output file.. {target_file}")
+    try:
+        save_to_new_file(df, file_path=str(target_file), sep="|")
+    except Exception as e:
+        logging.error(f"Error saving output file: {e}.")
+    if os.path.exists(target_file):
+        upload_file_to_gcs(
+            file_path=target_file,
+            target_gcs_bucket=target_gcs_bucket,
+            target_gcs_path=target_gcs_path,
+        )
+        table_exists = create_dest_table(
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=destination_table,
+            schema_filepath=schema_path,
+            bucket_name=target_gcs_bucket,
+            drop_table="N",
+        )
+        if table_exists:
+            load_data_to_bq(
+                project_id=project_id,
+                dataset_id=dataset_id,
+                table_id=destination_table,
+                file_path=target_file,
+                truncate_table=True,
+                field_delimiter="|",
+            )
+        else:
+            error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{destination_table} does not exist and/or could not be created."
+            raise ValueError(error_msg)
+    else:
+        logging.info(
+            f"Informational: The data file {target_file} was not generated because no data file was available.  Continuing."
+        )
 
-    df = create_geo_id(df, concat_col)
 
+def load_data_to_bq(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    file_path: str,
+    truncate_table: bool,
+    field_delimiter: str = "|",
+) -> None:
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} started"
+    )
+    client = bigquery.Client(project=project_id)
+    table_ref = client.dataset(dataset_id).table(table_id)
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.field_delimiter = field_delimiter
+    if truncate_table:
+        job_config.write_disposition = "WRITE_TRUNCATE"
+    else:
+        job_config.write_disposition = "WRITE_APPEND"
+    job_config.skip_leading_rows = 1  # ignore the header
+    job_config.autodetect = False
+    with open(file_path, "rb") as source_file:
+        job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+    job.result()
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} completed"
+    )
+
+
+def create_dest_table(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_filepath: list,
+    bucket_name: str,
+    drop_table: bool = False,
+) -> bool:
+    table_ref = f"{project_id}.{dataset_id}.{table_id}"
+    logging.info(f"Attempting to create table {table_ref} if it doesn't already exist")
+    client = bigquery.Client()
+    table_exists = False
+    try:
+        table = client.get_table(table_ref)
+        table_exists_id = table.table_id
+        logging.info(f"Table {table_exists_id} currently exists.")
+        if drop_table:
+            logging.info("Dropping existing table")
+            client.delete_table(table)
+            table = None
+    except NotFound:
+        table = None
+    if not table:
+        logging.info(
+            (
+                f"Table {table_ref} currently does not exist.  Attempting to create table."
+            )
+        )
+        if check_gcs_file_exists(schema_filepath, bucket_name):
+            schema = create_table_schema([], bucket_name, schema_filepath)
+            table = bigquery.Table(table_ref, schema=schema)
+            client.create_table(table)
+            print(f"Table {table_ref} was created".format(table_id))
+            table_exists = True
+        else:
+            file_name = os.path.split(schema_filepath)[1]
+            file_path = os.path.split(schema_filepath)[0]
+            logging.info(
+                f"Error: Unable to create table {table_ref} because schema file {file_name} does not exist in location {file_path} in bucket {bucket_name}"
+            )
+            table_exists = False
+    else:
+        table_exists = True
+    return table_exists
+
+
+def check_gcs_file_exists(file_path: str, bucket_name: str) -> bool:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    exists = storage.Blob(bucket=bucket, name=file_path).exists(storage_client)
+    return exists
+
+
+def create_table_schema(
+    schema_structure: list, bucket_name: str = "", schema_filepath: str = ""
+) -> list:
+    logging.info(f"Defining table schema... {bucket_name} ... {schema_filepath}")
+    schema = []
+    if not (schema_filepath):
+        schema_struct = schema_structure
+    else:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(schema_filepath)
+        schema_struct = json.loads(blob.download_as_string(client=None))
+    for schema_field in schema_struct:
+        fld_name = schema_field["name"]
+        fld_type = schema_field["type"]
+        try:
+            fld_descr = schema_field["description"]
+        except KeyError:
+            fld_descr = ""
+        fld_mode = schema_field["mode"]
+        schema.append(
+            bigquery.SchemaField(
+                name=fld_name, field_type=fld_type, mode=fld_mode, description=fld_descr
+            )
+        )
+    return schema
+
+
+def pivot_dataframe(
+    df: pd.DataFrame
+) -> pd.DataFrame:
     logging.info("Pivoting the dataframe...")
     df = df[["geo_id", "KPI_Name", "KPI_Value"]]
     df = df.pivot_table(
         index="geo_id", columns="KPI_Name", values="KPI_Value", aggfunc=np.sum
     ).reset_index()
-
-    logging.info("Reordering headers...")
-    df = df[headers]
-
-    logging.info(f"Saving to output file.. {target_file}")
-    try:
-        save_to_new_file(df, file_path=str(target_file))
-    except Exception as e:
-        logging.error(f"Error saving output file: {e}.")
-
-    logging.info(
-        f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
-    )
-    upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
-
-    logging.info(
-        f"ACS {pipeline_name} process completed at "
-        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    )
+    return df
 
 
 def string_replace(source_url, replace: dict) -> str:
@@ -173,13 +342,13 @@ def extract_data_and_convert_to_df_state_level(
             except OSError as e:
                 logging.info(f"error : {e}")
                 pass
-
     logging.info("creating the dataframe...")
     df = pd.concat(list_temp)
     return df
 
 
 def create_geo_id(df: pd.DataFrame, concat_col: str) -> pd.DataFrame:
+    logging.info("Creating column geo_id...")
     df["geo_id"] = ""
     for col in concat_col:
         df["geo_id"] = df["geo_id"] + df[col]
@@ -194,19 +363,31 @@ def pad_zeroes_to_the_left(val: str, length: int) -> str:
 
 
 def rename_headers(df: pd.DataFrame, rename_mappings: dict) -> None:
+    logging.info("Renaming headers...")
     rename_mappings = {int(k): str(v) for k, v in rename_mappings.items()}
     df.rename(columns=rename_mappings, inplace=True)
 
 
-def save_to_new_file(df: pd.DataFrame, file_path: str) -> None:
-    df.to_csv(file_path, index=False)
+def save_to_new_file(df: pd.DataFrame, file_path: str, sep: str = ",") -> None:
+    logging.info(f"Saving data to target file.. {file_path} ...")
+    df.to_csv(file_path, index=False, sep=sep)
 
 
-def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(gcs_bucket)
-    blob = bucket.blob(gcs_path)
-    blob.upload_from_filename(file_path)
+def upload_file_to_gcs(
+    file_path: pathlib.Path, target_gcs_bucket: str, target_gcs_path: str
+) -> None:
+    if os.path.exists(file_path):
+        logging.info(
+            f"Uploading output file to gs://{target_gcs_bucket}/{target_gcs_path}"
+        )
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(target_gcs_bucket)
+        blob = bucket.blob(target_gcs_path)
+        blob.upload_from_filename(file_path)
+    else:
+        logging.info(
+            f"Cannot upload file to gs://{target_gcs_bucket}/{target_gcs_path} as it does not exist."
+        )
 
 
 if __name__ == "__main__":
@@ -214,15 +395,21 @@ if __name__ == "__main__":
 
     main(
         source_url=os.environ["SOURCE_URL"],
-        year_report=os.environ["YEAR_REPORT"],
-        api_naming_convention=os.environ["API_NAMING_CONVENTION"],
         target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        project_id=os.environ["PROJECT_ID"],
+        dataset_id=os.environ["DATASET_ID"],
+        table_id=os.environ["TABLE_ID"],
+        schema_path=os.environ["SCHEMA_PATH"],
+        pipeline_name=os.environ["PIPELINE_NAME"],
         target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
         target_gcs_path=os.environ["TARGET_GCS_PATH"],
-        headers=json.loads(os.environ["CSV_HEADERS"]),
-        rename_mappings=json.loads(os.environ["RENAME_MAPPINGS"]),
-        pipeline_name=os.environ["PIPELINE_NAME"],
+        year_report=os.environ["YEAR_REPORT"],
+        api_naming_convention=os.environ["API_NAMING_CONVENTION"],
         geography=os.environ["GEOGRAPHY"],
         report_level=os.environ["REPORT_LEVEL"],
-        concat_col=json.loads(os.environ["CONCAT_COL"]),
+        group_id_file=os.environ["GROUP_ID_FILE"],
+        state_code_file=os.environ["STATE_CODE_FILE"],
+        concat_col_list=json.loads(os.environ["CONCAT_COL_LIST"]),
+        rename_mappings_list=json.loads(os.environ["RENAME_MAPPINGS_LIST"]),
+        input_csv_headers=json.loads(os.environ["INPUT_CSV_HEADERS"]),
     )
