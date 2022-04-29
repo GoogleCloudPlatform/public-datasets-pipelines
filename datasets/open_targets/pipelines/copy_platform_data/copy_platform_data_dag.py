687b8b04c5828615f7e6bbcb267b44b9d46f7e27
Adler Santos <adlersantos@google.com>
2022-04-29 18:14:49 -0400

    revised Airflow vars for platform data

--- datasets/open_targets/pipelines/copy_platform_data/copy_platform_data_dag.py
+++ datasets/open_targets/pipelines/copy_platform_data/copy_platform_data_dag.py
@@ -32,7 +32,7 @@ with DAG(
     default_view="graph",
 ) as dag:
 
-    # Transfer Open Targets Databases
+    # Transfer Open Targets Platform Dataset
     copy_bq_datasets = kubernetes_pod.KubernetesPodOperator(
         task_id="copy_bq_datasets",
         name="copy_bq_datasets",
@@ -41,10 +41,10 @@ with DAG(
         image_pull_policy="Always",
         image="{{ var.json.open_targets.container_registry.copy_bq_datasets }}",
         env_vars={
-            "SOURCE_PROJECT_ID": "{{ var.json.open_targets.source_project_id }}",
-            "TARGET_PROJECT_ID": "{{ var.json.open_targets.target_project_id }}",
+            "SOURCE_PROJECT_ID": "{{ var.json.open_targets.platform.source_project_id }}",
+            "TARGET_PROJECT_ID": "{{ var.json.open_targets.platform.target_project_id }}",
             "SERVICE_ACCOUNT": "{{ var.json.open_targets.service_account }}",
-            "TRANSFER_CONFIG_NAME": "open-targets",
+            "TRANSFER_CONFIG_NAME": "open-targets-platform",
             "SOURCE_DATASET_NAME": "{{ var.json.open_targets.platform.source_dataset_name }}",
             "TARGET_DATASET_NAME": "{{ var.json.open_targets.platform.target_dataset_name }}",
         },
