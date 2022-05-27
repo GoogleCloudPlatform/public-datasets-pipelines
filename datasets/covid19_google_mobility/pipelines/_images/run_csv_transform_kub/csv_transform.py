ddd3dac5d6a9a22bd70f47b10194d6f3d1291c34
Nicholas Large <84149918+nlarge-google@users.noreply.github.com>
2022-05-27 16:45:31 -0500

    fix: Revised COVID-19 Google Mobility dataset (#363)

--- datasets/covid19_google_mobility/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/covid19_google_mobility/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -16,98 +16,398 @@
 import datetime
 import json
 import logging
-import math
 import os
 import pathlib
 import typing
 
 import pandas as pd
 import requests
-from google.cloud import storage
+from google.api_core.exceptions import NotFound
+from google.cloud import bigquery, storage
 
 
 def main(
     source_url: str,
     source_file: pathlib.Path,
     target_file: pathlib.Path,
+    pipeline_name: str,
+    chunksize: str,
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
     target_gcs_bucket: str,
     target_gcs_path: str,
-    headers: typing.List[str],
-    rename_mappings: dict,
-    pipeline_name: str,
+    schema_path: str,
+    drop_dest_table: str,
+    input_field_delimiter: str,
+    remove_source_file: str,
+    delete_target_file: str,
+    input_csv_headers: typing.List[str],
+    data_dtypes: dict,
+    rename_headers_list: dict,
+    table_description: str,
 ) -> None:
-
-    logging.info(
-        f"COVID19 Google mobility {pipeline_name} process started at "
-        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    )
-
-    logging.info("Creating 'files' folder")
+    logging.info(f"{pipeline_name} process started")
     pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
+    execute_pipeline(
+        source_url=source_url,
+        source_file=source_file,
+        target_file=target_file,
+        chunksize=chunksize,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        destination_table=table_id,
+        target_gcs_bucket=target_gcs_bucket,
+        target_gcs_path=target_gcs_path,
+        schema_path=schema_path,
+        drop_dest_table=drop_dest_table,
+        input_field_delimiter=input_field_delimiter,
+        remove_source_file=remove_source_file,
+        delete_target_file=delete_target_file,
+        input_csv_headers=input_csv_headers,
+        data_dtypes=data_dtypes,
+        rename_headers_list=rename_headers_list,
+        table_description=table_description,
+    )
+    logging.info(f"{pipeline_name} process completed")
 
-    logging.info(f"Downloading file from {source_url}...")
+
+def execute_pipeline(
+    source_url: str,
+    source_file: pathlib.Path,
+    target_file: pathlib.Path,
+    chunksize: str,
+    project_id: str,
+    dataset_id: str,
+    destination_table: str,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+    schema_path: str,
+    drop_dest_table: str,
+    input_field_delimiter: str,
+    remove_source_file: str,
+    delete_target_file: str,
+    input_csv_headers: typing.List[str],
+    data_dtypes: dict,
+    rename_headers_list: dict,
+    table_description: str,
+) -> None:
     download_file(source_url, source_file)
-
-    logging.info(f"Opening file {source_file}...")
-    df = pd.read_csv(str(source_file))
-
-    logging.info(f"Transforming {source_file}... ")
-
-    logging.info(f"Transform: Rename columns.. {source_file}")
-    rename_headers(df, rename_mappings)
-
-    logging.info(f"Transform: Converting to integer {source_file}... ")
-    convert_values_to_integer_string(df)
-
-    logging.info("Transform: Reordering headers..")
-    df = df[headers]
-
-    logging.info(f"Saving to output file.. {target_file}")
-    try:
-        save_to_new_file(df, file_path=str(target_file))
-    except Exception as e:
-        logging.error(f"Error saving output file: {e}.")
-
-    logging.info(
-        f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
+    process_source_file(
+        source_url=source_url,
+        source_file=source_file,
+        target_file=target_file,
+        chunksize=chunksize,
+        input_headers=input_csv_headers,
+        data_dtypes=data_dtypes,
+        rename_headers_list=rename_headers_list,
+        header_row_ordinal="0",
+        field_separator=input_field_delimiter,
     )
-    upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
-
-    logging.info(
-        f"COVID19 Google mobility {pipeline_name} process completed at "
-        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    )
-
-
-def convert_to_integer_string(input: typing.Union[str, float]) -> str:
-    str_val = ""
-    if not input or (math.isnan(input)):
-        str_val = ""
+    if os.path.exists(target_file):
+        upload_file_to_gcs(
+            file_path=target_file,
+            target_gcs_bucket=target_gcs_bucket,
+            target_gcs_path=target_gcs_path,
+        )
+        table_exists = create_dest_table(
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=destination_table,
+            schema_filepath=schema_path,
+            bucket_name=target_gcs_bucket,
+            drop_table=(drop_dest_table == "Y"),
+            table_description=table_description,
+        )
+        if table_exists:
+            load_data_to_bq(
+                project_id=project_id,
+                dataset_id=dataset_id,
+                table_id=destination_table,
+                file_path=target_file,
+                truncate_table=True,
+                field_delimiter="|",
+            )
+            if remove_source_file == "Y":
+                os.remove(source_file)
+            else:
+                pass
+            if delete_target_file == "Y":
+                os.remove(target_file)
+            else:
+                pass
+        else:
+            error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{destination_table} does not exist and/or could not be created."
+            raise ValueError(error_msg)
     else:
-        str_val = str(int(round(input, 0)))
-    return str_val
+        logging.info(
+            f"Informational: The data file {target_file} was not generated because no data file was available.  Continuing."
+        )
 
 
-def convert_values_to_integer_string(df: pd.DataFrame) -> None:
-    cols = [
-        "retail_and_recreation_percent_change_from_baseline",
-        "grocery_and_pharmacy_percent_change_from_baseline",
-        "parks_percent_change_from_baseline",
-        "transit_stations_percent_change_from_baseline",
-        "workplaces_percent_change_from_baseline",
-        "residential_percent_change_from_baseline",
-    ]
+def process_source_file(
+    source_url: str,
+    source_file: str,
+    target_file: str,
+    chunksize: str,
+    input_headers: typing.List[str],
+    data_dtypes: dict,
+    rename_headers_list: typing.List[str],
+    header_row_ordinal: str = "0",
+    field_separator: str = ",",
+) -> None:
+    logging.info(f"Opening source file {source_file}")
+    if header_row_ordinal is None or header_row_ordinal == "None":
+        with pd.read_csv(
+            source_file,
+            engine="python",
+            encoding="utf-8",
+            quotechar='"',
+            chunksize=int(chunksize),  # size of batch data, in no. of records
+            sep=field_separator,  # data column separator, typically ","
+            names=input_headers,
+            dtype=data_dtypes,
+            keep_default_na=True,
+            na_values=[" "],
+        ) as reader:
+            for chunk_number, chunk in enumerate(reader):
+                target_file_batch = str(target_file).replace(
+                    ".csv", "-" + str(chunk_number) + ".csv"
+                )
+                df = pd.DataFrame()
+                df = pd.concat([df, chunk])
+                process_chunk(
+                    df=df,
+                    source_url=source_url,
+                    target_file_batch=target_file_batch,
+                    target_file=target_file,
+                    skip_header=(not chunk_number == 0),
+                    rename_headers_list=rename_headers_list,
+                )
+    else:
+        header = int(header_row_ordinal)
+        if data_dtypes != "[]":
+            with pd.read_csv(
+                source_file,
+                engine="python",
+                encoding="utf-8",
+                quotechar='"',
+                chunksize=int(chunksize),  # size of batch data, in no. of records
+                sep=field_separator,  # data column separator, typically ","
+                header=header,  # use when the data file does not contain a header
+                dtype=data_dtypes,
+                keep_default_na=True,
+                na_values=[" "],
+            ) as reader:
+                for chunk_number, chunk in enumerate(reader):
+                    target_file_batch = str(target_file).replace(
+                        ".csv", "-" + str(chunk_number) + ".csv"
+                    )
+                    df = pd.DataFrame()
+                    df = pd.concat([df, chunk])
+                    process_chunk(
+                        df=df,
+                        source_url=source_url,
+                        target_file_batch=target_file_batch,
+                        target_file=target_file,
+                        skip_header=(not chunk_number == 0),
+                        rename_headers_list=rename_headers_list,
+                    )
+        else:
+            with pd.read_csv(
+                source_file,
+                engine="python",
+                encoding="utf-8",
+                quotechar='"',
+                chunksize=int(chunksize),  # size of batch data, in no. of records
+                sep=field_separator,  # data column separator, typically ","
+                header=header,  # use when the data file does not contain a header
+                keep_default_na=True,
+                na_values=[" "],
+            ) as reader:
+                for chunk_number, chunk in enumerate(reader):
+                    target_file_batch = str(target_file).replace(
+                        ".csv", "-" + str(chunk_number) + ".csv"
+                    )
+                    df = pd.DataFrame()
+                    df = pd.concat([df, chunk])
+                    process_chunk(
+                        df=df,
+                        source_url=source_url,
+                        target_file_batch=target_file_batch,
+                        target_file=target_file,
+                        skip_header=(not chunk_number == 0),
+                        rename_headers_list=rename_headers_list,
+                    )
 
-    for cols in cols:
-        df[cols] = df[cols].apply(convert_to_integer_string)
+
+def process_chunk(
+    df: pd.DataFrame,
+    source_url: str,
+    target_file_batch: str,
+    target_file: str,
+    skip_header: bool,
+    rename_headers_list: typing.List[str],
+) -> None:
+    logging.info(f"Processing batch file {target_file_batch}")
+    df = rename_headers(df, rename_headers_list)
+    df = add_metadata_cols(df, source_url)
+    save_to_new_file(df, file_path=str(target_file_batch), sep="|")
+    append_batch_file(target_file_batch, target_file, skip_header, not (skip_header))
+    logging.info(f"Processing batch file {target_file_batch} completed")
 
 
-def rename_headers(df: pd.DataFrame, rename_mappings: dict) -> None:
-    df.rename(columns=rename_mappings, inplace=True)
+def load_data_to_bq(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    file_path: str,
+    truncate_table: bool,
+    field_delimiter: str = "|",
+) -> None:
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} started"
+    )
+    client = bigquery.Client(project=project_id)
+    table_ref = client.dataset(dataset_id).table(table_id)
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.field_delimiter = field_delimiter
+    if truncate_table:
+        job_config.write_disposition = "WRITE_TRUNCATE"
+    else:
+        job_config.write_disposition = "WRITE_APPEND"
+    job_config.skip_leading_rows = 1  # ignore the header
+    job_config.autodetect = False
+    with open(file_path, "rb") as source_file:
+        job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+    job.result()
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} completed"
+    )
 
 
-def save_to_new_file(df: pd.DataFrame, file_path: str) -> None:
-    df.to_csv(file_path, index=False)
+def create_dest_table(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_filepath: list,
+    bucket_name: str,
+    drop_table: bool = False,
+    table_description="",
+) -> bool:
+    table_ref = f"{project_id}.{dataset_id}.{table_id}"
+    logging.info(f"Attempting to create table {table_ref} if it doesn't already exist")
+    client = bigquery.Client()
+    table_exists = False
+    try:
+        table = client.get_table(table_ref)
+        table_exists_id = table.table_id
+        logging.info(f"Table {table_exists_id} currently exists.")
+        if drop_table:
+            logging.info("Dropping existing table")
+            client.delete_table(table)
+            table = None
+    except NotFound:
+        table = None
+    if not table:
+        logging.info(
+            (
+                f"Table {table_ref} currently does not exist.  Attempting to create table."
+            )
+        )
+        if check_gcs_file_exists(schema_filepath, bucket_name):
+            schema = create_table_schema([], bucket_name, schema_filepath)
+            table = bigquery.Table(table_ref, schema=schema)
+            table.description = table_description
+            client.create_table(table)
+            print(f"Table {table_ref} was created".format(table_id))
+            table_exists = True
+        else:
+            file_name = os.path.split(schema_filepath)[1]
+            file_path = os.path.split(schema_filepath)[0]
+            logging.info(
+                f"Error: Unable to create table {table_ref} because schema file {file_name} does not exist in location {file_path} in bucket {bucket_name}"
+            )
+            table_exists = False
+    else:
+        table_exists = True
+    return table_exists
+
+
+def check_gcs_file_exists(file_path: str, bucket_name: str) -> bool:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    exists = storage.Blob(bucket=bucket, name=file_path).exists(storage_client)
+    return exists
+
+
+def create_table_schema(
+    schema_structure: list, bucket_name: str = "", schema_filepath: str = ""
+) -> list:
+    logging.info(f"Defining table schema... {bucket_name} ... {schema_filepath}")
+    schema = []
+    if not (schema_filepath):
+        schema_struct = schema_structure
+    else:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(schema_filepath)
+        schema_struct = json.loads(blob.download_as_string(client=None))
+    for schema_field in schema_struct:
+        fld_name = schema_field["name"]
+        fld_type = schema_field["type"]
+        try:
+            fld_descr = schema_field["description"]
+        except KeyError:
+            fld_descr = ""
+        fld_mode = schema_field["mode"]
+        schema.append(
+            bigquery.SchemaField(
+                name=fld_name, field_type=fld_type, mode=fld_mode, description=fld_descr
+            )
+        )
+    return schema
+
+
+def rename_headers(df: pd.DataFrame, rename_mappings: dict) -> pd.DataFrame:
+    logging.info("Renaming Headers")
+    return df.rename(columns=rename_mappings)
+
+
+def add_metadata_cols(df: pd.DataFrame, source_url: str) -> pd.DataFrame:
+    logging.info("Adding metadata columns")
+    df["source_url"] = source_url
+    df["etl_timestamp"] = pd.to_datetime(
+        datetime.datetime.now(), format="%Y-%m-%d %H:%M:%S", infer_datetime_format=True
+    )
+    return df
+
+
+def save_to_new_file(df: pd.DataFrame, file_path: str, sep: str = "|") -> None:
+    logging.info(f"Saving data to target file.. {file_path} ...")
+    df.to_csv(file_path, index=False, sep=sep)
+
+
+def append_batch_file(
+    batch_file_path: str, target_file_path: str, skip_header: bool, truncate_file: bool
+) -> None:
+    with open(batch_file_path, "r") as data_file:
+        if truncate_file:
+            target_file = open(target_file_path, "w+").close()
+        with open(target_file_path, "a+") as target_file:
+            if skip_header:
+                logging.info(
+                    f"Appending batch file {batch_file_path} to {target_file_path} with skip header"
+                )
+                next(data_file)
+            else:
+                logging.info(
+                    f"Appending batch file {batch_file_path} to {target_file_path}"
+                )
+            target_file.write(data_file.read())
+            if os.path.exists(batch_file_path):
+                os.remove(batch_file_path)
 
 
 def download_file(source_url: str, source_file: pathlib.Path) -> None:
@@ -121,11 +421,21 @@ def download_file(source_url: str, source_file: pathlib.Path) -> None:
         logging.error(f"Couldn't download {source_url}: {r.text}")
 
 
-def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(gcs_bucket)
-    blob = bucket.blob(gcs_path)
-    blob.upload_from_filename(file_path)
+def upload_file_to_gcs(
+    file_path: pathlib.Path, target_gcs_bucket: str, target_gcs_path: str
+) -> None:
+    if os.path.exists(file_path):
+        logging.info(
+            f"Uploading output file to gs://{target_gcs_bucket}/{target_gcs_path}"
+        )
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(target_gcs_bucket)
+        blob = bucket.blob(target_gcs_path)
+        blob.upload_from_filename(file_path)
+    else:
+        logging.info(
+            f"Cannot upload file to gs://{target_gcs_bucket}/{target_gcs_path} as it does not exist."
+        )
 
 
 if __name__ == "__main__":
@@ -135,9 +445,20 @@ if __name__ == "__main__":
         source_url=os.environ["SOURCE_URL"],
         source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
         target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        pipeline_name=os.environ["PIPELINE_NAME"],
+        chunksize=os.environ["CHUNKSIZE"],
+        project_id=os.environ["PROJECT_ID"],
+        dataset_id=os.environ["DATASET_ID"],
+        table_id=os.environ["TABLE_ID"],
         target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
         target_gcs_path=os.environ["TARGET_GCS_PATH"],
-        headers=json.loads(os.environ["CSV_HEADERS"]),
-        rename_mappings=json.loads(os.environ["RENAME_MAPPINGS"]),
-        pipeline_name=os.environ["PIPELINE_NAME"],
+        schema_path=os.environ["SCHEMA_PATH"],
+        drop_dest_table=os.environ["DROP_DEST_TABLE"],
+        input_field_delimiter=os.environ["INPUT_FIELD_DELIMITER"],
+        remove_source_file=os.environ["REMOVE_SOURCE_FILE"],
+        delete_target_file=os.environ["DELETE_TARGET_FILE"],
+        input_csv_headers=json.loads(os.environ.get("INPUT_CSV_HEADERS", r"[]")),
+        data_dtypes=json.loads(os.environ.get("DATA_DTYPES", r"{}")),
+        rename_headers_list=json.loads(os.environ.get("RENAME_HEADERS_LIST", r"{}")),
+        table_description=os.environ.get("TABLE_DESCRIPTION", ""),
     )
