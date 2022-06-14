52471ff01e628c9e5222173b37d352614c62ed2a
gkodukula <gkodukula@google.com>
2022-06-14 18:47:51 +0000

    feat: onboarding mnist dataset, production ready

--- datasets/mnist/pipelines/mnist/mnist_dag.py
+++ datasets/mnist/pipelines/mnist/mnist_dag.py
@@ -37,6 +37,7 @@ with DAG(
         task_id="download_and_process_source_zip_file",
         name="mnist",
         namespace="composer",
+        service_account_name="datasets",
         image_pull_policy="Always",
         image="{{ var.json.mnist.container_registry.run_csv_transform_kub }}",
         env_vars={
@@ -45,6 +46,75 @@ with DAG(
             "TARGET_FILE": "files/t10k-images-idx3-ubyte.gz",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
             "TARGET_GCS_PATH": "data/mnist/mnist/t10k-images-idx3-ubyte.gz",
+            "PIPELINE_NAME": "mnist",
+        },
+        resources={
+            "request_memory": "2G",
+            "request_cpu": "200m",
+            "request_ephemeral_storage": "8G",
+        },
+    )
+
+    # Task to copy `train-images-idx3-ubyte.gz` from MNIST  Database to GCS
+    download_and_process_source_zip_file_2 = kubernetes_pod.KubernetesPodOperator(
+        task_id="download_and_process_source_zip_file_2",
+        name="mnist",
+        namespace="composer",
+        service_account_name="datasets",
+        image_pull_policy="Always",
+        image="{{ var.json.mnist.container_registry.run_csv_transform_kub }}",
+        env_vars={
+            "SOURCE_URL": "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz",
+            "SOURCE_FILE": "files/train-images-idx3-ubyte.gz",
+            "TARGET_FILE": "files/train-images-idx3-ubyte.gz",
+            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
+            "TARGET_GCS_PATH": "data/mnist/mnist/train-images-idx3-ubyte.gz",
+            "PIPELINE_NAME": "mnist",
+        },
+        resources={
+            "request_memory": "2G",
+            "request_cpu": "200m",
+            "request_ephemeral_storage": "8G",
+        },
+    )
+
+    # Task to copy `train-labels-idx1-ubyte.gz` from MNIST  Database to GCS
+    download_and_process_source_zip_file_3 = kubernetes_pod.KubernetesPodOperator(
+        task_id="download_and_process_source_zip_file_3",
+        name="mnist",
+        namespace="composer",
+        service_account_name="datasets",
+        image_pull_policy="Always",
+        image="{{ var.json.mnist.container_registry.run_csv_transform_kub }}",
+        env_vars={
+            "SOURCE_URL": "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
+            "SOURCE_FILE": "files/train-labels-idx1-ubyte.gz",
+            "TARGET_FILE": "files/train-labels-idx1-ubyte.gz",
+            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
+            "TARGET_GCS_PATH": "data/mnist/mnist/train-labels-idx1-ubyte.gz",
+            "PIPELINE_NAME": "mnist",
+        },
+        resources={
+            "request_memory": "2G",
+            "request_cpu": "200m",
+            "request_ephemeral_storage": "8G",
+        },
+    )
+
+    # Task to copy `t10k-labels-idx1-ubyte.gz` from MNIST  Database to GCS
+    download_and_process_source_zip_file_4 = kubernetes_pod.KubernetesPodOperator(
+        task_id="download_and_process_source_zip_file_4",
+        name="mnist",
+        namespace="composer",
+        service_account_name="datasets",
+        image_pull_policy="Always",
+        image="{{ var.json.mnist.container_registry.run_csv_transform_kub }}",
+        env_vars={
+            "SOURCE_URL": "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz",
+            "SOURCE_FILE": "files/t10k-labels-idx1-ubyte.gz",
+            "TARGET_FILE": "files/t10k-labels-idx1-ubyte.gz",
+            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
+            "TARGET_GCS_PATH": "data/mnist/mnist/t10k-labels-idx1-ubyte.gz",
             "PIPELINE_NAME": "mnist",
         },
         resources={
