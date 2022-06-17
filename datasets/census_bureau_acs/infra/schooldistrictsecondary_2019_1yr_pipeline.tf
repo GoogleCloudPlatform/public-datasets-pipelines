004516f68282266920d0d837c7335d5ba3d7063e
Naveen Noel <nnoel@google.com>
2022-06-17 12:21:47 +0000

    Fix: Debug and change

--- datasets/census_bureau_acs/infra/schooldistrictsecondary_2019_1yr_pipeline.tf
+++ datasets/census_bureau_acs/infra/schooldistrictsecondary_2019_1yr_pipeline.tf
@@ -16,15 +16,10 @@
 
 
 resource "google_bigquery_table" "census_bureau_acs_schooldistrictsecondary_2019_1yr" {
-  project    = var.project_id
-  dataset_id = "census_bureau_acs"
-  table_id   = "schooldistrictsecondary_2019_1yr"
-
+  project     = var.project_id
+  dataset_id  = "census_bureau_acs"
+  table_id    = "schooldistrictsecondary_2019_1yr"
   description = "School district secondary 2019 1 year report table"
-
-
-
-
   depends_on = [
     google_bigquery_dataset.census_bureau_acs
   ]
