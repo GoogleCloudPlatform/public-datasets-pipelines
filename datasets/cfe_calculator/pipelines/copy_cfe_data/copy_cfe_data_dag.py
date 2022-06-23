6922cb648703259e483c266bf6ef0af8ef9dbecd
nlarge-google <nlarge@google.com>
2022-06-23 14:51:07 +0000

    fix: Resolve dag id.

--- datasets/cfe_calculator/pipelines/copy_cfe_data/copy_cfe_data_dag.py
+++ datasets/cfe_calculator/pipelines/copy_cfe_data/copy_cfe_data_dag.py
@@ -24,7 +24,7 @@ default_args = {
 
 
 with DAG(
-    dag_id="cfe_calculator.cfe_calculator.cfe_calculator",
+    dag_id="cfe_calculator.cfe_calculator",
     default_args=default_args,
     max_active_runs=1,
     schedule_interval="@once",
