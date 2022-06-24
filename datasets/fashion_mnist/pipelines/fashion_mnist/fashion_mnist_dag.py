b71e9392be2b920cac2d96bf4d4b4d66bed029dd
gkodukula <gkodukula@google.com>
2022-06-24 13:15:42 +0000

    fix: used bash operator instead kpod

--- datasets/fashion_mnist/pipelines/fashion_mnist/fashion_mnist_dag.py
+++ datasets/fashion_mnist/pipelines/fashion_mnist/fashion_mnist_dag.py
@@ -14,7 +14,7 @@
 
 
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators import kubernetes_pod
+from airflow.operators import bash
 
 default_args = {
     "owner": "Google",
@@ -32,101 +32,17 @@ with DAG(
     default_view="graph",
 ) as dag:
 
-    # Task to copy `t10k-images-idx3-ubyte.gz` from FASHION MNIST Database to GCS
-    download_and_process_source_zip_file = kubernetes_pod.KubernetesPodOperator(
-        task_id="download_and_process_source_zip_file",
-        name="fashion_mnist",
-        namespace="composer",
-        service_account_name="datasets",
-        image_pull_policy="Always",
-        image="{{ var.json.fashion_mnist.container_registry.run_csv_transform_kub }}",
-        env_vars={
-            "SOURCE_URL": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz",
-            "SOURCE_FILE": "files/t10k-images-idx3-ubyte.gz",
-            "TARGET_FILE": "files/t10k-images-idx3-ubyte.gz",
-            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "data/fashion_mnist/fashion_mnist_test/t10k-images-idx3-ubyte.gz",
-            "PIPELINE_NAME": "fashion_mnist",
-        },
-        resources={
-            "request_memory": "2G",
-            "request_cpu": "200m",
-            "request_ephemeral_storage": "8G",
+    # Task to copy `fashion-mnist.gz` from FASHION MNIST Database to GCS
+    download_zip_files = bash.BashOperator(
+        task_id="download_zip_files",
+        bash_command="mkdir -p $data_dir/fashion-mnist\ncurl -o $data_dir/fashion-mnist/t10k-images-idx3-ubyte.gz -L $fashion_mnist_test\ncurl -o $data_dir/fashion-mnist/train-images-idx3-ubyte.gz -L $fashion_mnist_train\ncurl -o $data_dir/fashion-mnist/train-labels-idx1-ubyte.gz -L $fashion_mnist_train_labels\ncurl -o $data_dir/fashion-mnist/t10k-labels-idx1-ubyte.gz -L $fashion_mnist_test_labels\n",
+        env={
+            "data_dir": "/home/airflow/gcs/data/fashion-mnist",
+            "fashion_mnist_test": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz",
+            "fashion_mnist_train": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz",
+            "fashion_mnist_train_labels": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz",
+            "fashion_mnist_test_labels": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz",
         },
     )
 
-    # Task to copy `train-images-idx3-ubyte.gz` from FASHION MNIST  Database to GCS
-    download_and_process_source_zip_file_2 = kubernetes_pod.KubernetesPodOperator(
-        task_id="download_and_process_source_zip_file_2",
-        name="fashion_mnist",
-        namespace="composer",
-        service_account_name="datasets",
-        image_pull_policy="Always",
-        image="{{ var.json.fashion_mnist.container_registry.run_csv_transform_kub }}",
-        env_vars={
-            "SOURCE_URL": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz",
-            "SOURCE_FILE": "files/train-images-idx3-ubyte.gz",
-            "TARGET_FILE": "files/train-images-idx3-ubyte.gz",
-            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "data/fashion_mnist/fashion_mnist_train/train-images-idx3-ubyte.gz",
-            "PIPELINE_NAME": "fashion_mnist",
-        },
-        resources={
-            "request_memory": "2G",
-            "request_cpu": "200m",
-            "request_ephemeral_storage": "8G",
-        },
-    )
-
-    # Task to copy `train-labels-idx1-ubyte.gz` from FASHION MNIST  Database to GCS
-    download_and_process_source_zip_file_3 = kubernetes_pod.KubernetesPodOperator(
-        task_id="download_and_process_source_zip_file_3",
-        name="fashion_mnist",
-        namespace="composer",
-        service_account_name="datasets",
-        image_pull_policy="Always",
-        image="{{ var.json.fashion_mnist.container_registry.run_csv_transform_kub }}",
-        env_vars={
-            "SOURCE_URL": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz",
-            "SOURCE_FILE": "files/train-labels-idx1-ubyte.gz",
-            "TARGET_FILE": "files/train-labels-idx1-ubyte.gz",
-            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "data/fashion_mnist/fashion_mnist_train/train-labels-idx1-ubyte.gz",
-            "PIPELINE_NAME": "fashion_mnist",
-        },
-        resources={
-            "request_memory": "2G",
-            "request_cpu": "200m",
-            "request_ephemeral_storage": "8G",
-        },
-    )
-
-    # Task to copy `t10k-labels-idx1-ubyte.gz` from FASHION MNIST  Database to GCS
-    download_and_process_source_zip_file_4 = kubernetes_pod.KubernetesPodOperator(
-        task_id="download_and_process_source_zip_file_4",
-        name="fashion_mnist",
-        namespace="composer",
-        service_account_name="datasets",
-        image_pull_policy="Always",
-        image="{{ var.json.fashion_mnist.container_registry.run_csv_transform_kub }}",
-        env_vars={
-            "SOURCE_URL": "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz",
-            "SOURCE_FILE": "files/t10k-labels-idx1-ubyte.gz",
-            "TARGET_FILE": "files/t10k-labels-idx1-ubyte.gz",
-            "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "data/fashion_mnist/fashion_mnist_test/t10k-labels-idx1-ubyte.gz",
-            "PIPELINE_NAME": "fashion_mnist",
-        },
-        resources={
-            "request_memory": "2G",
-            "request_cpu": "200m",
-            "request_ephemeral_storage": "8G",
-        },
-    )
-
-    (
-        download_and_process_source_zip_file
-        >> download_and_process_source_zip_file_2
-        >> download_and_process_source_zip_file_3
-        >> download_and_process_source_zip_file_4
-    )
+    download_zip_files
