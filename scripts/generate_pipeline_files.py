9080e14727d66ba4533b6d3dfb8bf577b2ce5939
Alick Zhang <alick@google.com>
2022-07-22 11:38:32 -0700

    fix: code review changes + add test

--- scripts/generate_pipeline_files.py
+++ scripts/generate_pipeline_files.py
@@ -1,17 +1,33 @@
-import itertools
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+
 import json
 import os
 import pathlib
 
 import click
-import yaml
+from ruamel import yaml
+from ruamel.yaml.comments import CommentedMap
 
-# ignore yaml alias
-yaml.Dumper.ignore_aliases = lambda self, data: True
+yaml = yaml.YAML()
+yaml.representer.ignore_aliases = lambda *data: True
+
 CURRENT_PATH = pathlib.Path(__file__).resolve().parent
 PROJECT_ROOT = CURRENT_PATH.parent
 DATASETS_PATH = PROJECT_ROOT / "datasets"
 AIRFLOW_TEMPLATES_PATH = PROJECT_ROOT / "templates" / "airflow"
+
+license_header = (
+    pathlib.Path(AIRFLOW_TEMPLATES_PATH / "license_header.py.jinja2").read_text() + "\n"
+)
 
 
 @click.command()
@@ -36,35 +52,29 @@ def create_pipeline(dataset_id: str, pipeline_id: str):
     if not os.path.exists(dir):
         os.makedirs(dir)
         click.echo(
-            f"{DATASETS_PATH}/{dataset_id}/pipelines/{pipeline_id} has been created"
+            f"\n{DATASETS_PATH}/{dataset_id}/pipelines/{pipeline_id} has been created\n"
         )
     else:
-        click.echo(f"{new_pipeline_path} already exists")
-    create_dataset_yaml(dir, dataset_id)
-    create_pipeline_yaml(dir, dataset_id)
+        click.echo(f"\n{new_pipeline_path} already exists\n")
+    create_dataset_yaml(dataset_id)
+    create_pipeline_yaml(dir)
 
 
-def create_dataset_yaml(dir: str, dataset_id: str):
-    sample_yaml = None
+def create_dataset_yaml(dataset_id: str):
     output = {}
-    with open(f"{PROJECT_ROOT}/samples/dataset.yaml", "r") as sample_dataset_yaml:
-        try:
-            sample_yaml = yaml.safe_load(sample_dataset_yaml)
-            sample_yaml["dataset"]["name"] = dataset_id
-            dataset_desc = click.prompt(
-                "A user-friendly description of the dataset", type=str
-            )
-            sample_yaml["dataset"]["description"] = dataset_desc
-            output["dataset"] = sample_yaml["dataset"]
-        except yaml.YAMLError as exc:
-            print(exc)
-    resources = []
+    sample_yaml = yaml.load((PROJECT_ROOT / "samples" / "dataset.yaml").read_text())
+    sample_yaml["dataset"]["name"] = dataset_id
+    sample_yaml["dataset"]["friendly_name"] = dataset_id
+    dataset_desc = click.prompt("A user-friendly description of the dataset", type=str)
+    sample_yaml["dataset"]["description"] = dataset_desc
+    output["dataset"] = sample_yaml["dataset"]
 
-    if click.confirm("Will you need GCP Resource(s) for your pipeline?"):
+    resources = []
+    if click.confirm("\nWill you need GCP Resource(s) for your pipeline?"):
         while True:
             resource = click.prompt(
                 (
-                    "Which GCP Resource(s) are required for your pipeline\n"
+                    "\nWhich GCP Resource(s) are required for your pipeline\n"
                     "Select Resources Needed: BigQuery (BQ), Google Cloud Storage (GCS)?"
                 ),
                 type=click.Choice(["BQ", "GCS"], case_sensitive=False),
@@ -74,21 +84,21 @@ def create_dataset_yaml(dir: str, dataset_id: str):
                 bq_yaml = sample_yaml["resources"][0]
                 bq_yaml["dataset_id"] = dataset_id
                 bq_desc = click.prompt(
-                    "A user-friendly description of the dataset", type=str
+                    "\nA user-friendly description of the dataset", type=str
                 )
                 bq_yaml["description"] = bq_desc
                 resources.append(bq_yaml)
             if resource == "GCS":
                 gcs_yaml = sample_yaml["resources"][1]
                 gcs_bucket_name = click.prompt(
-                    "Your Cloud Storage Bucket Name\n"
+                    "\nYour Cloud Storage Bucket Name\n"
                     "Use hyphenated syntax, e.g. `some-prefix-123`, for the names.\n"
                     "Note that bucket names must not contain 'google' or close misspellings, such as 'g00gle'.",
                     type=str,
                 )
                 location = click.prompt(
                     (
-                        "The location of the bucket.\n"
+                        "\nThe location of the bucket.\n"
                         "Object data for objects in the bucket resides in physical storage within this region.\n"
                         "Defaults to US."
                     ),
@@ -98,71 +108,54 @@ def create_dataset_yaml(dir: str, dataset_id: str):
                 gcs_yaml["name"] = gcs_bucket_name
                 gcs_yaml["location"] = location
                 resources.append(gcs_yaml)
-            if not click.confirm("Would you like to add another resource"):
+            if not click.confirm("\nWould you like to add another resource"):
                 output["resources"] = resources
-                with open(f"{dir}/dataset.yaml", "w") as dataset_out:
-                    yaml.dump(output, dataset_out, Dumper=yaml.Dumper, sort_keys=False)
+                with open(
+                    f"{DATASETS_PATH}/{dataset_id}/pipelines/dataset.yaml", "w"
+                ) as dataset_out:
+                    dataset_out.write(license_header)
+                    yaml.dump(CommentedMap(output), dataset_out)
+                    click.echo(
+                        f"\n{DATASETS_PATH}/{dataset_id}/pipelines/dataset.yaml has been created\n"
+                    )
                 break
 
 
-def create_pipeline_yaml(dir: str, pipeline_id: str):
-    sample_yaml = None
+def create_pipeline_yaml(dir: str):
     output = {}
     resources = []
-    with open(f"{PROJECT_ROOT}/samples/pipeline.yaml", "r") as sample_pipeline_yaml:
-        try:
-            sample_yaml = yaml.safe_load(sample_pipeline_yaml)
-            tables = click.prompt(
-                "Input your BigQuery Table name(s) required for your pipeline\n"
-                "If you have multiple tables, please use a comma-seperated list. (eg. table1, table2, table3)"
-            )
-            for table_name in tables.split(","):
-                print(table_name)
-                sample_yaml["resources"][0]["table_id"] = table_name
-                bq_resource = sample_yaml["resources"][0]
-                resources.append(bq_resource.copy())
-            output["resources"] = resources
-
-        except yaml.YAMLError as exc:
-            print(exc)
+    sample_yaml = yaml.load((PROJECT_ROOT / "samples" / "pipeline.yaml").read_text())
+    tables = click.prompt(
+        "Input your BigQuery Table name(s) required for your pipeline\n"
+        "If you have multiple tables, please use a comma-seperated list. (eg. table1, table2, table3)"
+    )
+    for table_name in tables.split(","):
+        sample_yaml["resources"][0]["table_id"] = table_name.strip()
+        bq_resource = sample_yaml["resources"][0]
+        resources.append(bq_resource.copy())
+    output["resources"] = resources
 
     tasks = []
-    with open(f"{PROJECT_ROOT}/scripts/dag_imports.json", "r") as dag_imports:
-        airflow_operators = json.load(dag_imports)
-        airflow_version = click.prompt(
-            "Which Airflow Version does your pipeline use?",
-            type=click.Choice(["1", "2"], case_sensitive=False),
+    airflow_operators = json.loads(
+        (PROJECT_ROOT / "scripts" / "dag_imports.json").read_text()
+    )
+    operators = airflow_operators["2"]
+    while True:
+        operator = click.prompt(
+            "\nWhich operator would you like to add?",
+            type=click.Choice(list(operators), case_sensitive=False),
         )
-        if airflow_version == "1":
-            operators = airflow_operators[airflow_version]
-            while True:
-                operator = click.prompt(
-                    "Which operator would you like to add?",
-                    type=click.Choice(list(operators), case_sensitive=False),
-                )
-                t = [task["operator"] for task in sample_yaml["dag"]["tasks"]]
-                operator_idx = t.index(operator)
-                tasks.append(sample_yaml["dag"]["tasks"][operator_idx])
-                if not click.confirm("Would you like to add another operator?"):
-                    sample_yaml["dag"]["tasks"] = tasks
-                    output["dag"] = sample_yaml["dag"]
-                    print(output)
-                    break
-        if airflow_version == "2":
-            operators = airflow_operators[airflow_version]
-            while True:
-                operator = click.prompt(
-                    "Which operator would you like to add?",
-                    type=click.Choice(list(operators), case_sensitive=False),
-                )
-                t = [task["operator"] for task in sample_yaml["dag"]["tasks"]]
-                operator_idx = t.index(operator)
-                tasks.append(sample_yaml["dag"]["tasks"][operator_idx])
-                if not click.confirm("Would you like to add another operator?"):
-                    sample_yaml["dag"]["tasks"] = tasks
-                    output["dag"] = sample_yaml["dag"]
-                    print(output)
-                    break
+        t = [task["operator"] for task in sample_yaml["dag"]["tasks"]]
+        operator_idx = t.index(operator)
+        tasks.append(sample_yaml["dag"]["tasks"][operator_idx])
+        if not click.confirm("\nWould you like to add another operator?"):
+            sample_yaml["dag"]["tasks"] = tasks
+            output["dag"] = sample_yaml["dag"]
+            with open(f"{dir}/pipeline.yaml", "w") as pipeline_out:
+                pipeline_out.write(license_header)
+                yaml.dump(CommentedMap(output), pipeline_out)
+                click.echo(f"\n{dir}/pipeline.yaml has been created\n")
+            break
 
 
 if __name__ == "__main__":
---
+++ tests/scripts/test_generate_pipeline.py
@@ -0,0 +1,172 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+
+
+import pathlib
+import tempfile
+import typing
+
+import pytest
+from click.testing import CliRunner
+from ruamel import yaml
+
+from scripts import generate_dag, generate_pipeline_files, generate_terraform
+from scripts.generate_pipeline_files import create_pipeline
+
+yaml = yaml.YAML(typ="safe")
+
+PROJECT_ROOT = generate_pipeline_files.PROJECT_ROOT
+DATASETS_PATH = PROJECT_ROOT / "datasets"
+
+SAMPLE_YAML_PATHS = {
+    "dataset": PROJECT_ROOT / "samples" / "dataset.yaml",
+    "pipeline": PROJECT_ROOT / "samples" / "pipeline.yaml",
+}
+
+ENV_PATH = generate_pipeline_files.PROJECT_ROOT / ".test"
+ENV_DATASETS_PATH = ENV_PATH / "datasets"
+
+
+@pytest.fixture
+def env() -> str:
+    return "test"
+
+
+@pytest.fixture
+def dataset_path() -> typing.Iterator[pathlib.Path]:
+    with tempfile.TemporaryDirectory(
+        dir=generate_dag.DATASETS_PATH, suffix="_dataset"
+    ) as dir_path:
+        yield pathlib.Path(dir_path)
+
+
+@pytest.fixture
+def pipeline_path(
+    dataset_path: pathlib.Path, suffix="_pipeline"
+) -> typing.Iterator[pathlib.Path]:
+    pipelines_dir = dataset_path / "pipelines"
+    pipelines_dir.mkdir(parents=True, exist_ok=True)
+    with tempfile.TemporaryDirectory(dir=pipelines_dir, suffix=suffix) as dir_path:
+        yield pathlib.Path(dir_path)
+
+
+def all_pipelines() -> typing.Iterator[typing.Tuple[pathlib.Path, pathlib.Path]]:
+    for dataset_path_ in generate_terraform.list_subdirs(generate_dag.DATASETS_PATH):
+        for pipeline_path_ in generate_terraform.list_subdirs(
+            dataset_path_ / "pipelines"
+        ):
+            yield dataset_path_, pipeline_path_
+
+
+test_flow = {
+    "friendly_dataset_name": "my friendly dataset_description",
+    "resource_needed1": "y",
+    "resource1": "bq",
+    "bq_description": "dataset.yaml bq description",
+    "resource_needed2": "y",
+    "resource2": "gcs",
+    "gcs_bucket_name": "my-pipeline-test-bucket",
+    "gcs_bucket_location": "US",
+    "resource_needed3": "n",
+    "bq_tables": "table1, table2, table3",
+    "operators": "BashOperator",
+    "add_another_operator": "n",
+}
+
+
+def test_pipeline_directory():
+    runner = CliRunner()
+    runner.invoke(
+        create_pipeline,
+        "--dataset_id test_dataset --pipeline_id test_pipeline",
+        input="\n".join(list(test_flow.values())),
+    )
+
+    assert (DATASETS_PATH / "test_dataset" / "pipelines" / "test_pipeline").exists()
+    assert (DATASETS_PATH / "test_dataset" / "pipelines" / "test_pipeline").is_dir()
+    assert (DATASETS_PATH / "test_dataset" / "pipelines").exists()
+    assert (DATASETS_PATH / "test_dataset" / "pipelines").is_dir()
+
+
+def test_dataset_yaml_exists():
+    runner = CliRunner()
+    runner.invoke(
+        create_pipeline,
+        "--dataset_id test_dataset --pipeline_id test_pipeline",
+        input="\n".join(list(test_flow.values())),
+    )
+    assert (DATASETS_PATH / "test_dataset" / "pipelines" / "dataset.yaml").exists()
+    assert (DATASETS_PATH / "test_dataset" / "pipelines" / "dataset.yaml").is_file()
+
+
+def test_dataset_yaml_content():
+    runner = CliRunner()
+    runner.invoke(
+        create_pipeline,
+        "--dataset_id test_dataset --pipeline_id test_pipeline",
+        input="\n".join(list(test_flow.values())),
+    )
+    dataset_yaml_file = (
+        DATASETS_PATH / "test_dataset" / "pipelines" / "dataset.yaml"
+    ).read_text()
+    dataset_yaml = yaml.load(dataset_yaml_file)
+    license_header = (
+        PROJECT_ROOT / "templates" / "airflow" / "license_header.py.jinja2"
+    ).read_text()
+
+    assert license_header in dataset_yaml_file  # test for license header
+    assert (
+        len(dataset_yaml["resources"]) == 2
+    )  # test dataset yaml has 2 resources added (bq, gcs)
+    assert "dataset" in list(
+        dataset_yaml.keys()
+    )  # confirm keys of yaml file are correct
+    assert "resources" in list(
+        dataset_yaml.keys()
+    )  # confirm keys of yaml file are correct
+
+
+def test_pipeline_yaml_exists():
+    runner = CliRunner()
+    runner.invoke(
+        create_pipeline,
+        "--dataset_id test_dataset --pipeline_id test_pipeline",
+        input="\n".join(list(test_flow.values())),
+    )
+    assert (
+        DATASETS_PATH / "test_dataset" / "pipelines" / "test_pipeline" / "pipeline.yaml"
+    ).exists()
+    assert (
+        DATASETS_PATH / "test_dataset" / "pipelines" / "test_pipeline" / "pipeline.yaml"
+    ).is_file()
+
+
+def test_pipeline_yaml_content():
+    runner = CliRunner()
+    runner.invoke(
+        create_pipeline,
+        "--dataset_id test_dataset --pipeline_id test_pipeline",
+        input="\n".join(list(test_flow.values())),
+    )
+    pipeline_yaml_file = (
+        DATASETS_PATH / "test_dataset" / "pipelines" / "test_pipeline" / "pipeline.yaml"
+    ).read_text()
+    pipeline_yaml = yaml.load(pipeline_yaml_file)
+    license_header = (
+        PROJECT_ROOT / "templates" / "airflow" / "license_header.py.jinja2"
+    ).read_text()
+
+    assert license_header in pipeline_yaml_file  # test for license header
+    assert len(pipeline_yaml["resources"]) == 3  # test pipeline yaml has 3 bq resources
+    assert len(pipeline_yaml["dag"]["tasks"]) == 1  # confirm single task has been added
+    assert (
+        "BashOperator" == pipeline_yaml["dag"]["tasks"][0]["operator"]
+    )  # confirm BashOperator was added
