3e8d20dd90f29a8daeaa7d2b34af8360fe383759
Nicholas Large <84149918+nlarge-google@users.noreply.github.com>
2022-08-09 15:31:27 -0500

    Fix: Resolve cluster name mismatch in pipeline.yaml (#439)

--- datasets/census_bureau_acs/pipelines/census_bureau_acs/census_bureau_acs_dag.py
+++ datasets/census_bureau_acs/pipelines/census_bureau_acs/census_bureau_acs_dag.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -2860,7 +2860,7 @@ with DAG(
         task_id="delete_cluster",
         project_id="{{ var.value.gcp_project }}",
         location="us-central1-c",
-        name="census_bureau_acs",
+        name="census-bureau-acs",
     )
 
     (
