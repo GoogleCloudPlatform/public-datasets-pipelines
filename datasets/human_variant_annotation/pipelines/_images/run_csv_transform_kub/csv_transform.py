899fdcbb2544dc2b8a6832d2e5b8d123752da6eb
Siddam Vijay Kumar Reddy <siddamv@google.com>
2022-08-09 09:15:01 +0000

    fix: function annotation for csv_transform.py

--- datasets/human_variant_annotation/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/human_variant_annotation/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -16,19 +16,57 @@ import datetime
 import logging
 import os
 import pathlib
+import typing
 
 import requests
 from google.cloud import storage
 
 
-def main(base_url, folder, version, gcs_bucket, target_gcs_folder, pipeline):
+def main(
+    base_url: str,
+    folder: pathlib.Path,
+    version: str,
+    gcs_bucket: str,
+    target_gcs_folder: str,
+    pipeline: str,
+):
     logging.info(
         f"Human Variant Annotation Dataset {pipeline} pipeline process started at "
         + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     )
     logging.info(f"Creating './files/{folder}'")
     pathlib.Path(f"./files/{folder}").mkdir(parents=True, exist_ok=True)
-    date_time = datetime.datetime.now()
+    dates = get_dates()
+    for date in dates:
+        date_time = datetime.datetime.strptime(date, "%Y%m%d")
+        get_files(date_time, base_url, version, folder, gcs_bucket, target_gcs_folder)
+
+    logging.info(
+        f"Human Variant Annotation Dataset {pipeline} pipeline process completed at "
+        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    )
+
+
+def get_dates() -> typing.List[str]:
+    today = datetime.datetime.now()
+    start_date = datetime.datetime(today.year, today.month - 1, 1)
+    end_date = today
+    delta = datetime.timedelta(days=1)
+    dates = []
+    while start_date <= end_date:
+        dates.append(start_date.strftime("%Y%m%d"))
+        start_date += delta
+    return dates
+
+
+def get_files(
+    date_time: datetime.datetime,
+    base_url: str,
+    version: str,
+    folder: pathlib.Path,
+    gcs_bucket: str,
+    target_gcs_folder: str,
+):
     file_name = f"clinvar_{date_time.strftime('%Y%m%d')}.vcf.gz"
     source_url = base_url + f"archive_{version}/{date_time.strftime('%Y')}/{file_name}"
     source_file = f"./files/{folder}/{file_name}"
@@ -36,10 +74,6 @@ def main(base_url, folder, version, gcs_bucket, target_gcs_folder, pipeline):
     if status_code == 200:
         target_gcs_path = f"{target_gcs_folder}{file_name}"
         upload_file_to_gcs(source_file, gcs_bucket, target_gcs_path)
-    logging.info(
-        f"Human Variant Annotation Dataset {pipeline} pipeline process completed at "
-        + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    )
 
 
 def download_gzfile(source_url: str, source_file: str):
@@ -49,11 +83,9 @@ def download_gzfile(source_url: str, source_file: str):
         with open(source_file, "wb") as fb:
             for chunk in res:
                 fb.write(chunk)
-        logging.info(f"Downloaded data from {source_url} into {source_file}")
+        logging.info(f"\tDownloaded data from {source_url} into {source_file}")
     else:
-        logging.info(
-            f"\n\tCouldn't download {source_url}: Error {res.status_code}\n**** No new data added to FTP(source url) from last 24 hours **** "
-        )
+        logging.info(f"Couldn't download {source_url}: Error {res.status_code}")
     return res.status_code
 
 
