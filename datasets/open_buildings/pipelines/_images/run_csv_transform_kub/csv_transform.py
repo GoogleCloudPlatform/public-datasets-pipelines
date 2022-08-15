b707dda794abd0b5069eedb85e901161e4c6560e
Naveen Noel <nnoel@google.com>
2022-08-15 13:12:56 +0000

    Fix: code debug

--- datasets/open_buildings/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/open_buildings/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import logging
 import os
@@ -7,12 +21,7 @@ from google.api_core.exceptions import NotFound
 from google.cloud import bigquery, storage
 
 
-def main(
-    source_gcs_path,
-    project_id,
-    dataset_id,
-    gcs_bucket,
-    schema_filepath) ->None:
+def main(source_gcs_path, project_id, dataset_id, gcs_bucket, schema_filepath):
     logging.info("Creating 'files' folder")
     pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
     print()
