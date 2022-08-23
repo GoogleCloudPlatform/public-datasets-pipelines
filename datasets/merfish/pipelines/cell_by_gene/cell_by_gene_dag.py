851add5cc9810be87353bce8ba8c775380111e2c
gkodukula <gkodukula@google.com>
2022-08-23 18:57:19 +0000

    fix: added pipelines, production ready

--- datasets/merfish/pipelines/cell_by_gene/cell_by_gene_dag.py
+++ datasets/merfish/pipelines/cell_by_gene/cell_by_gene_dag.py
@@ -24,7 +24,7 @@ default_args = {
 
 
 with DAG(
-    dag_id="merfish.merfish",
+    dag_id="merfish.cell_by_gene",
     default_args=default_args,
     max_active_runs=1,
     schedule_interval="@weekly",
@@ -1755,119 +1755,4 @@ with DAG(
         ],
     )
 
-    # Task to load CSV data to a BigQuery table
-    load_cell_metadata_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
-        task_id="load_cell_metadata_to_bq",
-        bucket="public-datasets-vizgen-merfish",
-        source_objects=[
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate1/cell_metadata_S1R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate2/cell_metadata_S1R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate3/cell_metadata_S1R3.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate1/cell_metadata_S2R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate2/cell_metadata_S2R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate3/cell_metadata_S2R3.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate1/cell_metadata_S3R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate2/cell_metadata_S3R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate3/cell_metadata_S3R3.csv",
-        ],
-        source_format="CSV",
-        destination_project_dataset_table="bigquery-public-data-dev.merfish.cell_metadata",
-        skip_leading_rows=1,
-        allow_quoted_newlines=True,
-        write_disposition="WRITE_TRUNCATE",
-        schema_fields=[
-            {"name": "fov", "type": "string", "description": None, "mode": "nullable"},
-            {
-                "name": "volume",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "center_x",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "center_y",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "min_x",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "min_y",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "max_y",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-        ],
-    )
-
-    # Task to load CSV data to a BigQuery table
-    load_detected_transcripts_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
-        task_id="load_detected_transcripts_to_bq",
-        bucket="public-datasets-vizgen-merfish",
-        source_objects=[
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate1/detected_transcripts_S1R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate2/detected_transcripts_S1R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice1/Replicate3/detected_transcripts_S1R3.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate1/detected_transcripts_S2R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate2/detected_transcripts_S2R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice2/Replicate3/detected_transcripts_S2R3.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate1/detected_transcripts_S3R1.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate2/detected_transcripts_S3R2.csv",
-            "datasets/mouse_brain_map/BrainReceptorShowcase/Slice3/Replicate3/detected_transcripts_S3R3.csv",
-        ],
-        source_format="CSV",
-        destination_project_dataset_table="bigquery-public-data-dev.merfish.detected_transcripts",
-        skip_leading_rows=1,
-        allow_quoted_newlines=True,
-        write_disposition="WRITE_TRUNCATE",
-        schema_fields=[
-            {
-                "name": "barcode_id",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "global_x",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "global_y",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {
-                "name": "global_z",
-                "type": "string",
-                "description": None,
-                "mode": "nullable",
-            },
-            {"name": "x", "type": "string", "description": None, "mode": "nullable"},
-            {"name": "y", "type": "string", "description": None, "mode": "nullable"},
-            {"name": "fov", "type": "string", "description": None, "mode": "nullable"},
-            {"name": "gene", "type": "string", "description": None, "mode": "nullable"},
-        ],
-    )
-
     load_cell_by_gene_to_bq
-    load_cell_metadata_to_bq
-    load_detected_transcripts_to_bq
