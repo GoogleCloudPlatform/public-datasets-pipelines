7e7963cde4fc9cab9c39477601d00b02a01ec168
Naveen Noel <nnoel@google.com>
2022-09-21 15:13:13 +0000

    Code

---
+++ datasets/world_bank_wdi/pipelines/world_bank_wdi/world_bank_wdi_dag.py
@@ -0,0 +1,41 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from airflow import DAG
+from airflow.operators import bash
+
+default_args = {
+    "owner": "Google",
+    "depends_on_past": False,
+    "start_date": "2022-09-21",
+}
+
+
+with DAG(
+    dag_id="world_bank_wdi.world_bank_wdi",
+    default_args=default_args,
+    max_active_runs=1,
+    schedule_interval="@daily",
+    catchup=False,
+    default_view="graph",
+) as dag:
+
+    # Copy data gcs to gcs
+    bash_gcs_to_gcs = bash.BashOperator(
+        task_id="bash_gcs_to_gcs",
+        bash_command=" gsutil cp gs://pdp-feeds-staging/RelayWorldBank/WDI_csv/WDICountry-Series.csv gs://{{ var.value.composer_bucket }}/data/world_bank_wdi/raw_files/ ; gsutil cp gs://pdp-feeds-staging/RelayWorldBank/WDI_csv/WDICountry.csv gs://{{ var.value.composer_bucket }}/data/world_bank_wdi/raw_files/ ; gsutil cp gs://pdp-feeds-staging/RelayWorldBank/WDI_csv/WDIFootNote.csv gs://{{ var.value.composer_bucket }}/data/world_bank_wdi/raw_files/ ; gsutil cp gs://pdp-feeds-staging/RelayWorldBank/WDI_csv/WDISeries.csv gs://{{ var.value.composer_bucket }}/data/world_bank_wdi/raw_files/ ; gsutil cp gs://pdp-feeds-staging/RelayWorldBank/WDI_csv/WDISeries-Time.csv gs://{{ var.value.composer_bucket }}/data/world_bank_wdi/raw_files/ ; ",
+    )
+
+    bash_gcs_to_gcs
