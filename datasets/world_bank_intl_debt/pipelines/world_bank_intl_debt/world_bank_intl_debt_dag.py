d16454222555487a69f22724adf0aef809e6833c
Naveen Noel <nnoel@google.com>
2022-10-06 14:47:40 +0000

    Code: world_bank_intl_debt

--- datasets/world_bank_intl_debt/pipelines/world_bank_intl_debt/world_bank_intl_debt_dag.py
+++ datasets/world_bank_intl_debt/pipelines/world_bank_intl_debt/world_bank_intl_debt_dag.py
@@ -26,7 +26,7 @@ default_args = {
 
 
 with DAG(
-    dag_id="world_bank_health_population.world_bank_health_population",
+    dag_id="world_bank_intl_debt.world_bank_intl_debt",
     default_args=default_args,
     max_active_runs=1,
     schedule_interval="@daily",
@@ -37,7 +37,7 @@ with DAG(
     # Copy the source files
     copy_gcs_to_gcs1 = bash.BashOperator(
         task_id="copy_gcs_to_gcs1",
-        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/hnp_stats_csv/HNP_StatsCountry-Series.csv gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/ ;",
+        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/IDS_CSV/IDSCountry-Series.csv gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/ ;",
     )
 
     # Run CSV transform within kubernetes pod
@@ -48,15 +48,15 @@ with DAG(
         namespace="composer",
         service_account_name="datasets",
         image_pull_policy="Always",
-        image="{{ var.json.world_bank_health_population.container_registry.run_csv_transform_kub }}",
+        image="{{ var.json.world_bank_intl_debt.container_registry.run_csv_transform_kub }}",
         env_vars={
-            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/HNP_StatsCountry-Series.csv",
-            "SOURCE_FILE": "files/HNP_StatsCountry-Series.csv",
+            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/IDSCountry-Series.csv",
+            "SOURCE_FILE": "files/IDSCountry-Series.csv",
             "PROJECT_ID": "{{ var.value.gcp_project }}",
-            "COLUMN_TO_REMOVE": "{{ var.json.world_bank_health_population.country_series_definitions.column_to_remove }}",
+            "COLUMN_TO_REMOVE": "Unnamed: 3",
             "TARGET_FILE": "files/data_output.csv",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "{{ var.json.world_bank_health_population.country_series_definitions.target_gcs_path }}",
+            "TARGET_GCS_PATH": "data/world_bank_intl_debt/country_series_definitions/data_output.csv",
             "PIPELINE_NAME": "country_series_definitions",
             "CSV_HEADERS": '["country_code" ,"series_code" ,"description"]',
             "RENAME_MAPPINGS": '{"CountryCode":"country_code","SeriesCode":"series_code","DESCRIPTION":"description"}',
@@ -67,9 +67,9 @@ with DAG(
     country_series_definitions_load_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
         task_id="country_series_definitions_load_to_bq",
         bucket="{{ var.value.composer_bucket }}",
-        source_objects="{{ var.json.world_bank_health_population.country_series_definitions.source_objects }}",
+        source_objects="data/world_bank_intl_debt/country_series_definitions/data_output.csv",
         source_format="CSV",
-        destination_project_dataset_table="world_bank_health_population.country_series_definitions",
+        destination_project_dataset_table="world_bank_intl_debt.country_series_definitions",
         skip_leading_rows=1,
         write_disposition="WRITE_TRUNCATE",
         schema_fields=[
@@ -82,7 +82,7 @@ with DAG(
     # Copy the source files
     copy_gcs_to_gcs2 = bash.BashOperator(
         task_id="copy_gcs_to_gcs2",
-        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/hnp_stats_csv/HNP_StatsCountry.csv gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/ ;",
+        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/IDS_CSV/IDSCountry.csv gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/ ;",
     )
 
     # Run CSV transform within kubernetes pod
@@ -93,17 +93,17 @@ with DAG(
         namespace="composer",
         service_account_name="datasets",
         image_pull_policy="Always",
-        image="{{ var.json.world_bank_health_population.container_registry.run_csv_transform_kub }}",
+        image="{{ var.json.world_bank_intl_debt.container_registry.run_csv_transform_kub }}",
         env_vars={
-            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/HNP_StatsCountry.csv",
-            "SOURCE_FILE": "files/HNP_StatsCountry.csv",
-            "COLUMN_TO_REMOVE": "{{ var.json.world_bank_health_population.country_summary.column_to_remove }}",
+            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/IDSCountry.csv",
+            "SOURCE_FILE": "files/IDSCountry.csv",
+            "COLUMN_TO_REMOVE": "Unnamed: 31",
             "TARGET_FILE": "files/data_output.csv",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "{{ var.json.world_bank_health_population.country_summary.target_gcs_path }}",
+            "TARGET_GCS_PATH": "data/world_bank_intl_debt/country_summary/data_output.csv",
             "PIPELINE_NAME": "country_summary",
-            "CSV_HEADERS": '["country_code","short_name","table_name","long_name","two_alpha_code","currency_unit","special_notes","region","income_group","wb_2_code","national_accounts_base_year","national_accounts_reference_year","sna_price_valuation","lending_category","other_groups","system_of_national_accounts","alternative_conversion_factor","ppp_survey_year","balance_of_payments_manual_in_use","external_debt_reporting_status","system_of_trade","government_accounting_concept","imf_data_dissemination_standard","latest_population_census","latest_household_survey","source_of_most_recent_income_and_expenditure_data","vital_registration_complete","latest_agricultural_census","latest_industrial_data","latest_trade_data"]',
-            "RENAME_MAPPINGS": '{"Country Code":"country_code","Short Name":"short_name","Table Name":"table_name","Long Name":"long_name","2-alpha code":"two_alpha_code","Currency Unit":"currency_unit","Special Notes":"special_notes","Region":"region","Income Group":"income_group","WB-2 code":"wb_2_code","National accounts base year":"national_accounts_base_year","National accounts reference year":"national_accounts_reference_year","SNA price valuation":"sna_price_valuation","Lending category":"lending_category","Other groups":"other_groups","System of National Accounts":"system_of_national_accounts","Alternative conversion factor":"alternative_conversion_factor","PPP survey year":"ppp_survey_year","Balance of Payments Manual in use":"balance_of_payments_manual_in_use","External debt Reporting status":"external_debt_reporting_status","System of trade":"system_of_trade","Government Accounting concept":"government_accounting_concept","IMF data dissemination standard":"imf_data_dissemination_standard","Latest population census":"latest_population_census","Latest household survey":"latest_household_survey","Source of most recent Income and expenditure data":"source_of_most_recent_income_and_expenditure_data","Vital registration complete":"vital_registration_complete","Latest agricultural census":"latest_agricultural_census","Latest industrial data":"latest_industrial_data","Latest trade data":"latest_trade_data"}',
+            "CSV_HEADERS": '["country_code","short_name","table_name","long_name","two_alpha_code","currency_unit","special_notes","region","income_group","wb_2_code","national_accounts_base_year","national_accounts_reference_year","sna_price_valuation","lending_category","other_groups","system_of_national_accounts","alternative_conversion_factor","ppp_survey_year","balance_of_payments_manual_in_use","external_debt_reporting_status","system_of_trade","government_accounting_concept","imf_data_dissemination_standard","latest_population_census","latest_household_survey","source_of_most_recent_Income_and_expenditure_data","vital_registration_complete","latest_agricultural_census","latest_industrial_data","latest_trade_data","latest_water_withdrawal_data"]',
+            "RENAME_MAPPINGS": '{"Country Code":"country_code","Short Name":"short_name","Table Name":"table_name","Long Name":"long_name","2-alpha code":"two_alpha_code","Currency Unit":"currency_unit","Special Notes":"special_notes","Region":"region","Income Group":"income_group","WB-2 code":"wb_2_code","National accounts base year":"national_accounts_base_year","National accounts reference year":"national_accounts_reference_year","SNA price valuation":"sna_price_valuation","Lending category":"lending_category","Other groups":"other_groups","System of National Accounts":"system_of_national_accounts","Alternative conversion factor":"alternative_conversion_factor","PPP survey year":"ppp_survey_year","Balance of Payments Manual in use":"balance_of_payments_manual_in_use","External debt Reporting status":"external_debt_reporting_status","System of trade":"system_of_trade","Government Accounting concept":"government_accounting_concept","IMF data dissemination standard":"imf_data_dissemination_standard","Latest population census":"latest_population_census","Latest household survey":"latest_household_survey","Source of most recent Income and expenditure data":"source_of_most_recent_Income_and_expenditure_data","Vital registration complete":"vital_registration_complete","Latest agricultural census":"latest_agricultural_census","Latest industrial data":"latest_industrial_data","Latest trade data":"latest_trade_data,"Latest water withdrawal data":"latest_water_withdrawal_data"}',
         },
     )
 
@@ -111,9 +111,9 @@ with DAG(
     country_summary_load_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
         task_id="country_summary_load_to_bq",
         bucket="{{ var.value.composer_bucket }}",
-        source_objects="{{ var.json.world_bank_health_population.country_summary.source_objects }}",
+        source_objects="data/world_bank_intl_debt/country_summary/data_output.csv",
         source_format="CSV",
-        destination_project_dataset_table="world_bank_health_population.country_summary",
+        destination_project_dataset_table="world_bank_intl_debt.country_summary",
         skip_leading_rows=1,
         allow_quoted_newlines=True,
         write_disposition="WRITE_TRUNCATE",
@@ -176,7 +176,7 @@ with DAG(
             {"name": "latest_population_census", "type": "string", "mode": "nullable"},
             {"name": "latest_household_survey", "type": "string", "mode": "nullable"},
             {
-                "name": "source_of_most_recent_income_and_expenditure_data",
+                "name": "source_of_most_recent_Income_and_expenditure_data",
                 "type": "string",
                 "mode": "nullable",
             },
@@ -192,13 +192,18 @@ with DAG(
             },
             {"name": "latest_industrial_data", "type": "integer", "mode": "nullable"},
             {"name": "latest_trade_data", "type": "integer", "mode": "nullable"},
+            {
+                "name": "latest_water_withdrawal_data",
+                "type": "integer",
+                "mode": "nullable",
+            },
         ],
     )
 
     # Copy the source files
     copy_gcs_to_gcs3 = bash.BashOperator(
         task_id="copy_gcs_to_gcs3",
-        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/hnp_stats_csv/HNP_StatsSeries.csv gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/ ;",
+        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/IDS_CSV/IDSSeries.csv gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/ ;",
     )
 
     # Run CSV transform within kubernetes pod
@@ -209,14 +214,14 @@ with DAG(
         namespace="composer",
         service_account_name="datasets",
         image_pull_policy="Always",
-        image="{{ var.json.world_bank_health_population.container_registry.run_csv_transform_kub }}",
+        image="{{ var.json.world_bank_intl_debt.container_registry.run_csv_transform_kub }}",
         env_vars={
-            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/HNP_StatsSeries.csv",
-            "SOURCE_FILE": "files/HNP_StatsSeries.csv",
-            "COLUMN_TO_REMOVE": "{{ var.json.world_bank_health_population.series_summary.column_to_remove }}",
+            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/IDSSeries.csv",
+            "SOURCE_FILE": "files/IDSSeries.csv",
+            "COLUMN_TO_REMOVE": "Unnamed: 20",
             "TARGET_FILE": "files/data_output.csv",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "{{ var.json.world_bank_health_population.series_summary.target_gcs_path }}",
+            "TARGET_GCS_PATH": "data/world_bank_intl_debt/series_summary/data_output.csv",
             "PIPELINE_NAME": "series_summary",
             "CSV_HEADERS": '["series_code" ,"topic" ,"indicator_name" ,"short_definition" ,"long_definition" ,"unit_of_measure" ,"periodicity" ,"base_period" ,"other_notes" ,"aggregation_method" ,"limitations_and_exceptions" ,"notes_from_original_source" ,"general_comments" ,"source" ,"statistical_concept_and_methodology" ,"development_relevance" ,"related_source_links" ,"other_web_links" ,"related_indicators" ,"license_type"]',
             "RENAME_MAPPINGS": '{"Series Code":"series_code" ,"Topic":"topic" ,"Indicator Name":"indicator_name" ,"Short definition":"short_definition" ,"Long definition":"long_definition" ,"Unit of measure":"unit_of_measure" ,"Periodicity":"periodicity" ,"Base Period":"base_period" ,"Other notes":"other_notes" ,"Aggregation method":"aggregation_method" ,"Limitations and exceptions":"limitations_and_exceptions" ,"Notes from original source":"notes_from_original_source" ,"General comments":"general_comments" ,"Source":"source" ,"Statistical concept and methodology":"statistical_concept_and_methodology" ,"Development relevance":"development_relevance" ,"Related source links":"related_source_links" ,"Other web links":"other_web_links" ,"Related indicators":"related_indicators" ,"License Type":"license_type"}',
@@ -227,9 +232,9 @@ with DAG(
     series_summary_load_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
         task_id="series_summary_load_to_bq",
         bucket="{{ var.value.composer_bucket }}",
-        source_objects="{{ var.json.world_bank_health_population.series_summary.source_objects }}",
+        source_objects="data/world_bank_intl_debt/series_summary/data_output.csv",
         source_format="CSV",
-        destination_project_dataset_table="world_bank_health_population.series_summary",
+        destination_project_dataset_table="world_bank_intl_debt.series_summary",
         skip_leading_rows=1,
         allow_quoted_newlines=True,
         write_disposition="WRITE_TRUNCATE",
@@ -241,7 +246,7 @@ with DAG(
             {"name": "long_definition", "type": "string", "mode": "nullable"},
             {"name": "unit_of_measure", "type": "string", "mode": "nullable"},
             {"name": "periodicity", "type": "string", "mode": "nullable"},
-            {"name": "base_period", "type": "integer", "mode": "nullable"},
+            {"name": "base_period", "type": "string", "mode": "nullable"},
             {"name": "other_notes", "type": "string", "mode": "nullable"},
             {"name": "aggregation_method", "type": "string", "mode": "nullable"},
             {
@@ -272,7 +277,7 @@ with DAG(
     # Copy the source files
     copy_gcs_to_gcs4 = bash.BashOperator(
         task_id="copy_gcs_to_gcs4",
-        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/hnp_stats_csv/HNP_StatsSeries-Time.csv gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/ ;",
+        bash_command="gsutil cp gs://pdp-feeds-staging/RelayWorldBank/IDS_CSV/IDSSeries-Time.csv gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/ ;",
     )
 
     # Run CSV transform within kubernetes pod
@@ -283,14 +288,14 @@ with DAG(
         namespace="composer",
         service_account_name="datasets",
         image_pull_policy="Always",
-        image="{{ var.json.world_bank_health_population.container_registry.run_csv_transform_kub }}",
+        image="{{ var.json.world_bank_intl_debt.container_registry.run_csv_transform_kub }}",
         env_vars={
-            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_health_population/raw_files/HNP_StatsSeries-Time.csv",
-            "SOURCE_FILE": "files/HNP_StatsSeries-Time.csv",
-            "COLUMN_TO_REMOVE": "{{ var.json.world_bank_health_population.series_times.column_to_remove }}",
+            "SOURCE_URL": "gs://{{ var.value.composer_bucket }}/data/world_bank_intl_debt/raw_files/IDSSeries-Time.csv",
+            "SOURCE_FILE": "files/IDSSeries-Time.csv",
+            "COLUMN_TO_REMOVE": "Unnamed: 3",
             "TARGET_FILE": "files/data_output.csv",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
-            "TARGET_GCS_PATH": "{{ var.json.world_bank_health_population.series_times.target_gcs_path }}",
+            "TARGET_GCS_PATH": "data/world_bank_intl_debt/series_times/data_output.csv",
             "PIPELINE_NAME": "series_times",
             "CSV_HEADERS": '["series_code","year","description"]',
             "RENAME_MAPPINGS": '{"SeriesCode" : "series_code" ,"Year" : "year" ,"DESCRIPTION" : "description"}',
@@ -301,9 +306,9 @@ with DAG(
     series_times_load_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
         task_id="series_times_load_to_bq",
         bucket="{{ var.value.composer_bucket }}",
-        source_objects="{{ var.json.world_bank_health_population.series_times.source_objects }}",
+        source_objects="data/world_bank_intl_debt/series_times/data_output.csv",
         source_format="CSV",
-        destination_project_dataset_table="world_bank_health_population.series_times",
+        destination_project_dataset_table="world_bank_intl_debt.series_times",
         skip_leading_rows=1,
         write_disposition="WRITE_TRUNCATE",
         schema_fields=[
