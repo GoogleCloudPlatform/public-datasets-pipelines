333a982b669e418ce711850a905c90e4dd7f3ce5
Naveen Noel <nnoel@google.com>
2022-11-07 13:31:48 +0000

    Code

--- datasets/covid19_jhu_csse/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/covid19_jhu_csse/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -2,18 +2,29 @@ import json
 import logging
 import os
 
+import pandas as pd
 from google.api_core.exceptions import NotFound
 from google.cloud import bigquery, storage
 
-download_path="/usr/local/google/home/nnoel/Downloads/csv_transform_dump/"
-schema_dict={}
-def main(source_gcs_path, destination_gcs_path, project_id, dataset_id, gcs_bucket, schema_filepath) -> None:
+
+def main(
+    download_path,
+    source_gcs_path,
+    destination_gcs_path,
+    project_id,
+    dataset_id,
+    gcs_bucket,
+    schema_filepath,
+    rename_mappings,
+    add_header,
+) -> None:
     source_file_names = fetch_gcs_file_names(source_gcs_path, gcs_bucket)
     for each_file in source_file_names:
         pipeline_name = each_file
-        table_id = each_file[:-5].replace(".","_")
+        table_id = each_file[:-4]
         logging.info(f"Started Extraction and Load process for {pipeline_name} --->")
         execute_pipeline(
+            download_path,
             source_gcs_path,
             destination_gcs_path,
             project_id,
@@ -22,10 +33,11 @@ def main(source_gcs_path, destination_gcs_path, project_id, dataset_id, gcs_buck
             pipeline_name,
             table_id,
             schema_filepath,
+            rename_mappings,
+            add_header,
         )
         logging.info(f"Finished process for {pipeline_name}")
         print()
-        # break
 
 
 def fetch_gcs_file_names(source_gcs_path, gcs_bucket):
@@ -33,39 +45,55 @@ def fetch_gcs_file_names(source_gcs_path, gcs_bucket):
     blobs = client.list_blobs(gcs_bucket, prefix=source_gcs_path)
     source_file_names = []
     for blob in blobs:
-        if blob.name.endswith(".json"):
+        if blob.name.endswith(".csv"):
             source_file_names.append(blob.name.split("/")[-1])
     logging.info(f"{len(source_file_names)} tables to be loaded in bq")
     return source_file_names
 
 
 def execute_pipeline(
+    download_path,
     source_gcs_path,
     destination_gcs_path,
     project_id,
     dataset_id,
-    gcs_bucket,                        #pipeline name has the extension, table_id doesnt
+    gcs_bucket,
     pipeline_name,
     table_id,
     schema_filepath,
+    rename_mappings,
+    add_header,
 ):
     logging.info(f"ETL started for {pipeline_name}")
-    pipeline_name=download_file(source_gcs_path, gcs_bucket, pipeline_name)
-    table_id=replace_hyphen(table_id)
-    if check_file(pipeline_name):
-        schema_fields=transform_file(pipeline_name)
-        prepare_schema_dict(pipeline_name, schema_fields, schema_dict)
-        upload_transformed_file(destination_gcs_path, gcs_bucket, pipeline_name)
+    pipeline_name = download_file(
+        download_path, source_gcs_path, gcs_bucket, pipeline_name
+    )
+    if check_file(download_path, pipeline_name):
+        schema_fields, final_filename = transform_file(
+            download_path, pipeline_name, rename_mappings, add_header
+        )
+        schema_dict = prepare_schema_dict(table_id, schema_fields, {})
+        prepare_upload_schema_file(
+            download_path,
+            gcs_bucket,
+            destination_gcs_path,
+            schema_filepath,
+            schema_dict,
+        )
+        upload_transformed_file(
+            destination_gcs_path, gcs_bucket, final_filename, pipeline_name
+        )
         client = storage.Client()
-        blob = client.list_blobs(gcs_bucket, prefix=source_gcs_path + pipeline_name)
+        blob = client.list_blobs(
+            gcs_bucket, prefix=destination_gcs_path + pipeline_name
+        )
         if blob:
             table_exists = create_dest_table(
-                pipeline_name=pipeline_name,
                 project_id=project_id,
                 dataset_id=dataset_id,
                 table_id=table_id,
-                gcs_bucket=gcs_bucket,
                 schema_filepath=schema_filepath,
+                schema_dict=schema_dict,
                 drop_table=True,
             )
             if table_exists:
@@ -76,8 +104,6 @@ def execute_pipeline(
                     table_id=table_id,
                     gcs_bucket=gcs_bucket,
                     source_gcs_path=destination_gcs_path,
-                    truncate_table=True,
-                    field_delimiter="|",
                 )
             else:
                 error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{table_id} does not exist and/or could not be created."
@@ -88,64 +114,96 @@ def execute_pipeline(
         logging.info(f"File {pipeline_name} empty/corrupted")
 
 
-def check_file(pipeline_name):
-    with open(download_path+pipeline_name) as f:
+def check_file(download_path, pipeline_name):
+    with open(download_path + pipeline_name) as f:
         return f.read()
 
-def transform_file(pipeline_name):
-    logging.info("Fetching schema from the file itself.")
-    schema_fields=get_schema_fields(pipeline_name)
+
+def transform_file(download_path, pipeline_name, rename_mappings, add_header):
+    df = pd.read_csv(download_path + pipeline_name)
     logging.info("Transforming file")
-    with open(download_path+pipeline_name, "r+") as f:
-        # file_data=json.load(f)
-        for row in f:
-            for field in schema_fields:
-                if not json.loads(row).get(field):
-                    json.loads(row)[field]=''
-                    # file_data[pipeline_name].append({field:''})
-        
-    return schema_fields
+    rename_headers(df, rename_mappings)
+    rename_date_headers(df, rename_mappings)
+    add_location_header(df, add_header)
+    reorder_headers(df, add_header)
+    final_filename = save_to_file(df, download_path, pipeline_name)
+    return list(df.columns), final_filename
 
 
-def get_schema_fields(pipeline_name):
-    max_no_cols=0
-    with open(download_path+pipeline_name) as f:
-        for row in f:
-            num_of_cols=len(json.loads(row).keys())
-            if num_of_cols>max_no_cols:
-                max_no_cols=num_of_cols
-                row_with_schema=row
-    return list(json.loads(row_with_schema).keys())
+def save_to_file(df, download_path, pipeline_name):
+    filename_prefix = "final_"
+    filename = download_path + filename_prefix + pipeline_name
+    df.to_csv(filename, index=False)
+    return filename
 
-def prepare_schema_dict(pipeline_name, schema_fields, schema_dict):    # '{pipeline_name:[{},{}],pipeline_name:[{},{}]}'
-    schema_dict[pipeline_name]=[]
+
+def reorder_headers(df, add_header):
+    logging.info("Reordering headers")
+    col = df.pop(add_header)
+    df.insert(4, col.name, col)
+
+
+def add_location_header(df, add_header):
+    logging.info("Adding a new column by concatenating 2")
+    df[add_header] = (
+        "POINT(" + df["latitude"].astype(str) + " " + df["longitude"].astype(str) + ")"
+    )
+
+
+def rename_date_headers(df, rename_mappings):
+    logging.info("Make date headers BigQuery friendly")
+    cols = list(df.columns)
+    date_cols = []
+    for i in cols:
+        if i not in list(rename_mappings.values()):
+            date_cols.append(i)
+    date_dict = {}
+    for date in date_cols:
+        date_dict[date] = "_" + date.replace("/", "_")
+    rename_headers(df, date_dict)
+
+
+def rename_headers(df, rename_mappings):
+    logging.info("Rename the headers")
+    df.rename(columns=rename_mappings, inplace=True)
+
+
+def prepare_schema_dict(table_id, schema_fields, schema_dict):
+    schema_dict[table_id] = []
     for i in schema_fields:
-        schema_dict[pipeline_name].append({"name": i,"type": "STRING","mode": "NULLABLE"})
-    # return schema_dict
+        schema_dict[table_id].append({"name": i, "type": "STRING", "mode": "NULLABLE"})
+    return schema_dict
 
 
-def replace_hyphen(x):
-    return x.replace("-","_")
+def prepare_upload_schema_file(
+    download_path, gcs_bucket, destination_gcs_path, schema_filepath, schema_dict
+):
+    logging.info("Preparing schema file")
+    with open(download_path + schema_filepath, "w") as file:
+        json.dump(schema_dict, file)
+    logging.info("Uploading schema file to GCS")
+    client = storage.Client()
+    bucket = client.bucket(gcs_bucket)
+    destination_gcs_path = destination_gcs_path[:22]
+    blob = bucket.blob(destination_gcs_path + schema_filepath)
+    blob.upload_from_filename(download_path + schema_filepath)
 
 
-def download_file(source_gcs_path, gcs_bucket, pipeline_name):
-    client=storage.Client()
-    bucket=client.bucket(gcs_bucket)
-    blob=bucket.blob(source_gcs_path+pipeline_name)
-    # blob.download_to_filename(pipeline_name)
-    pipeline_name=replace_hyphen(pipeline_name)
-    pipeline_name=pipeline_name.replace(".","_",1)
-    blob.download_to_filename(download_path+pipeline_name)
+def download_file(download_path, source_gcs_path, gcs_bucket, pipeline_name):
+    client = storage.Client()
+    bucket = client.bucket(gcs_bucket)
+    blob = bucket.blob(source_gcs_path + pipeline_name)
+    blob.download_to_filename(download_path + pipeline_name)
     logging.info(f"Finished downloading {pipeline_name}")
     return pipeline_name
 
+
 def create_dest_table(
-    pipeline_name: str,
     project_id: str,
     dataset_id: str,
     table_id: str,
-    gcs_bucket: str,
     schema_filepath: str,
+    schema_dict,
     drop_table: bool,
 ) -> bool:
     table_ref = f"{project_id}.{dataset_id}.{table_id}"
@@ -166,7 +224,7 @@ def create_dest_table(
             f"Table {table_ref} currently does not exist.  Attempting to create table."
         )
         if schema_filepath:
-            schema = create_table_schema(schema_filepath, schema_dict, pipeline_name)
+            schema = create_table_schema(schema_dict, table_id)
             table = bigquery.Table(table_ref, schema=schema)
             client.create_table(table)
             logging.info(f"Table {table_id} was created")
@@ -179,14 +237,10 @@ def create_dest_table(
     return table_exists
 
 
-def create_table_schema(schema_filepath, schema_dict, pipeline_name) -> list:
+def create_table_schema(schema_dict, table_id) -> list:
     logging.info("Defining table schema")
     schema = []
-    # with open(schema_filepath) as f:
-    #     sc = f.read()
-    # schema_struct = json.loads(sc)
-
-    for schema_field in schema_dict[pipeline_name]:
+    for schema_field in schema_dict[table_id]:
         fld_name = schema_field["name"]
         fld_type = schema_field["type"]
         try:
@@ -201,12 +255,15 @@ def create_table_schema(schema_filepath, schema_dict, pipeline_name) -> list:
         )
     return schema
 
-def upload_transformed_file(destination_gcs_path, gcs_bucket, pipeline_name):
+
+def upload_transformed_file(
+    destination_gcs_path, gcs_bucket, final_filename, pipeline_name
+):
     logging.info("Uploading file to GCS")
-    client=storage.Client()
-    bucket=client.bucket(gcs_bucket)
-    blob=bucket.blob(destination_gcs_path+pipeline_name)
-    blob.upload_from_filename(download_path+pipeline_name)
+    client = storage.Client()
+    bucket = client.bucket(gcs_bucket)
+    blob = bucket.blob(destination_gcs_path + pipeline_name)
+    blob.upload_from_filename(final_filename)
 
 
 def load_data_to_bq(
@@ -216,17 +273,17 @@ def load_data_to_bq(
     table_id: str,
     gcs_bucket: str,
     source_gcs_path: str,
-    truncate_table: bool,
-    field_delimiter: str = "|",
 ) -> None:
     logging.info(
         f"Loading output data from {source_gcs_path} into {project_id}.{dataset_id}.{table_id} ...."
     )
     client = bigquery.Client(project=project_id)
     table_ref = f"{project_id}.{dataset_id}.{table_id}"
-    job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON)
+    job_config = bigquery.LoadJobConfig(
+        skip_leading_rows=1, source_format=bigquery.SourceFormat.CSV
+    )
     job = client.load_table_from_uri(
-        f"gs://{gcs_bucket}/{source_gcs_path}{table_id}.json",
+        f"gs://{gcs_bucket}/{source_gcs_path}{pipeline_name}",
         table_ref,
         job_config=job_config,
     )
@@ -237,11 +294,13 @@ def load_data_to_bq(
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     main(
+        download_path=os.environ.get("DOWNLOAD_PATH", ""),
         source_gcs_path=os.environ.get("SOURCE_GCS_PATH"),
         destination_gcs_path=os.environ.get("DESTINATION_GCS_PATH"),
         project_id=os.environ.get("PROJECT_ID"),
         dataset_id=os.environ.get("DATASET_ID"),
         gcs_bucket=os.environ.get("GCS_BUCKET"),
         schema_filepath=os.environ.get("SCHEMA_FILEPATH"),
-        # download_path=os.environ.get("DOWNLOAD_PATH")
+        rename_mappings=json.loads(os.environ.get("RENAME_MAPPINGS")),
+        add_header=os.environ.get("ADD_HEADER"),
     )
