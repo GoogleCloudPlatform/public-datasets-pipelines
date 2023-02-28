29d1dd00244e53ad99c7805725917a13ee2c3638
nlarge-google <nlarge@google.com>
2023-02-28 00:04:23 +0000

    Next revision.  Creates table and attempts to load data from file.

--- datasets/nppes/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/nppes/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -17,26 +17,29 @@ import json
 import logging
 import os
 import pathlib
-import typing
-
-import pandas as pd
+import re
 import requests
-from google.cloud import storage
+from zipfile import ZipFile
+from google.api_core.exceptions import NotFound
+from google.cloud import bigquery, storage
 
 
 def main(
     source_url: str,
     source_file: pathlib.Path,
-    # target_file: pathlib.Path,
-    # target_gcs_bucket: str,
-    # target_gcs_path: str,
-    # headers: typing.List[str],
-    # rename_mappings: dict,
-    # pipeline_name: str,
+    target_file: pathlib.Path,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_path: str,
+    source_npi_data_file_regexp: str,
+    pipeline_name: str,
 ) -> None:
 
     logging.info(
-        f"Load process started at {str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}"
+        f"{pipeline_name} load process started at {str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}"
     )
     logging.info("Creating 'files' folder")
     pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
@@ -48,36 +51,154 @@ def main(
     src_zip_file = f"{os.path.dirname(source_file)}/{os.path.basename(src_url)}"
     # download_file(src_url, src_zip_file)
 
-    # logging.info(f"Opening file {source_file}")
-    # df = pd.read_csv(str(source_file))
-
-    # rename_headers(df, rename_mappings)
-    # df = df[headers]
-
-    # logging.info(f"Transformation Process complete .. {source_file}")
-    # logging.info(f"Saving to output file.. {target_file}")
-
-    # try:
-    #     save_to_new_file(df, file_path=str(target_file))
-    # except Exception as e:
-    #     logging.error(f"Error saving output file: {e}.")
-
-    # logging.info(
-    #     f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
-    # )
-    # upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
-
+    logging.info(f"Searching for source NPI data file within {src_zip_file}")
+    with ZipFile(src_zip_file, 'r') as src_zip:
+        listOfFileNames = src_zip.namelist()
+        for fileName in listOfFileNames:
+            if re.match(rf'{source_npi_data_file_regexp}', fileName):
+                logging.info(f"Found data file {fileName}, extracting ...")
+                # src_zip.extract(fileName, os.path.dirname(target_file))
+                target_file = f"{os.path.dirname(target_file)}/{fileName}"
+                # if os.path.exists(target_file):
+                #     upload_file_to_gcs(
+                #         file_path=target_file,
+                #         target_gcs_bucket=target_gcs_bucket,
+                #         target_gcs_path=target_gcs_path,
+                #     )
+                table_exists = create_dest_table(
+                    project_id=project_id,
+                    dataset_id=dataset_id,
+                    table_id=table_id,
+                    schema_filepath=schema_path,
+                    bucket_name=target_gcs_bucket,
+                    drop_table=False,
+                )
+                if table_exists:
+                    load_data_to_bq(
+                        project_id=project_id,
+                        dataset_id=dataset_id,
+                        table_id=table_id,
+                        file_path=target_file,
+                        truncate_table=True,
+                        field_delimiter=",",
+                    )
+                else:
+                    error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{destination_table} does not exist and/or could not be created."
+                    raise ValueError(error_msg)
     logging.info(
-        f"Load process completed at {str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}"
+        f"{pipeline_name} load process completed at {str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}"
     )
 
 
-def rename_headers(df: pd.DataFrame, rename_mappings: dict) -> None:
-    df.rename(columns=rename_mappings, inplace=True)
+def load_data_to_bq(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    file_path: str,
+    truncate_table: bool,
+    field_delimiter: str = "|",
+) -> None:
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} started"
+    )
+    client = bigquery.Client(project=project_id)
+    table_ref = client.dataset(dataset_id).table(table_id)
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.field_delimiter = field_delimiter
+    if truncate_table:
+        job_config.write_disposition = "WRITE_TRUNCATE"
+    else:
+        job_config.write_disposition = "WRITE_APPEND"
+    job_config.skip_leading_rows = 1  # ignore the header
+    job_config.autodetect = False
+    with open(file_path, "rb") as source_file:
+        job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+    job.result()
+    logging.info(
+        f"Loading data from {file_path} into {project_id}.{dataset_id}.{table_id} completed"
+    )
 
 
-def save_to_new_file(df: pd.DataFrame, file_path: str) -> None:
-    df.to_csv(file_path, float_format="%.0f", index=False)
+def create_dest_table(
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_filepath: list,
+    bucket_name: str,
+    drop_table: bool = False,
+) -> bool:
+    table_ref = f"{project_id}.{dataset_id}.{table_id}"
+    logging.info(f"Attempting to create table {table_ref} if it doesn't already exist")
+    client = bigquery.Client()
+    table_exists = False
+    try:
+        table = client.get_table(table_ref)
+        table_exists_id = table.table_id
+        logging.info(f"Table {table_exists_id} currently exists.")
+        if drop_table:
+            logging.info("Dropping existing table")
+            client.delete_table(table)
+            table = None
+    except NotFound:
+        table = None
+    if not table:
+        logging.info(
+            (
+                f"Table {table_ref} currently does not exist.  Attempting to create table."
+            )
+        )
+        if check_gcs_file_exists(schema_filepath, bucket_name):
+            schema = create_table_schema([], bucket_name, schema_filepath)
+            table = bigquery.Table(table_ref, schema=schema)
+            client.create_table(table)
+            print(f"Table {table_ref} was created".format(table_id))
+            table_exists = True
+        else:
+            file_name = os.path.split(schema_filepath)[1]
+            file_path = os.path.split(schema_filepath)[0]
+            logging.info(
+                f"Error: Unable to create table {table_ref} because schema file {file_name} does not exist in location {file_path} in bucket {bucket_name}"
+            )
+            table_exists = False
+    else:
+        table_exists = True
+    return table_exists
+
+
+def check_gcs_file_exists(file_path: str, bucket_name: str) -> bool:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    exists = storage.Blob(bucket=bucket, name=file_path).exists(storage_client)
+    return exists
+
+
+def create_table_schema(
+    schema_structure: list, bucket_name: str = "", schema_filepath: str = ""
+) -> list:
+    logging.info(f"Defining table schema... {bucket_name} ... {schema_filepath}")
+    schema = []
+    if not (schema_filepath):
+        schema_struct = schema_structure
+    else:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(schema_filepath)
+        schema_struct = json.loads(blob.download_as_string(client=None))
+    for schema_field in schema_struct:
+        fld_name = schema_field["name"]
+        fld_type = schema_field["type"]
+        try:
+            fld_descr = schema_field["description"]
+        except KeyError:
+            fld_descr = ""
+        fld_mode = schema_field["mode"]
+        schema.append(
+            bigquery.SchemaField(
+                name=fld_name, field_type=fld_type, mode=fld_mode, description=fld_descr
+            )
+        )
+    return schema
 
 
 def download_file(source_url: str, source_file: pathlib.Path) -> None:
@@ -91,11 +212,21 @@ def download_file(source_url: str, source_file: pathlib.Path) -> None:
         logging.error(f"Couldn't download {source_url}: {r.text}")
 
 
-def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(gcs_bucket)
-    blob = bucket.blob(gcs_path)
-    blob.upload_from_filename(file_path)
+def upload_file_to_gcs(
+    file_path: pathlib.Path, target_gcs_bucket: str, target_gcs_path: str
+) -> None:
+    if os.path.exists(file_path):
+        logging.info(
+            f"Uploading output file to gs://{target_gcs_bucket}/{target_gcs_path}"
+        )
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(target_gcs_bucket)
+        blob = bucket.blob(target_gcs_path)
+        blob.upload_from_filename(file_path)
+    else:
+        logging.info(
+            f"Cannot upload file to gs://{target_gcs_bucket}/{target_gcs_path} as it does not exist."
+        )
 
 
 if __name__ == "__main__":
@@ -104,10 +235,13 @@ if __name__ == "__main__":
     main(
         source_url=os.environ["SOURCE_URL"],
         source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
-        # target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
-        # target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
-        # target_gcs_path=os.environ["TARGET_GCS_PATH"],
-        # headers=json.loads(os.environ["CSV_HEADERS"]),
-        # rename_mappings=json.loads(os.environ["RENAME_MAPPINGS"]),
-        # pipeline_name=os.environ["PIPELINE_NAME"],
+        target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
+        target_gcs_path=os.environ["TARGET_GCS_PATH"],
+        source_npi_data_file_regexp=os.environ["SOURCE_NPI_DATA_FILE_REGEXP"],
+        pipeline_name=os.environ["PIPELINE_NAME"],
+        project_id=os.environ.get("PROJECT_ID", ""),
+        dataset_id=os.environ.get("DATASET_ID", ""),
+        table_id=os.environ.get("TABLE_ID", ""),
+        schema_path=os.environ.get("SCHEMA_PATH", ""),
     )
