237fcbe540f242be985ef0e98981a44206a2b700
nlarge-google <nlarge@google.com>
2023-03-07 07:01:04 +0000

    fix: batches and utilizes bash.

--- datasets/nppes/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/nppes/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -13,15 +13,17 @@
 # limitations under the License.
 
 import datetime
+import gzip
 import json
 import logging
-import numpy as np
 import os
 import pathlib
 import re
+import subprocess
 import typing
-from zipfile import ZipFile, Path
+from zipfile import Path, ZipFile
 
+import numpy as np
 import requests
 from dateutil.relativedelta import relativedelta
 from google.api_core.exceptions import NotFound
@@ -48,8 +50,7 @@ def main(
     )
     logging.info("Creating destination folder")
     pathlib.Path(f"{os.path.dirname(source_file)}").mkdir(parents=True, exist_ok=True)
-    # src_zip_file = download_source_file(source_url, source_file)
-    src_zip_file = "/usr/local/google/home/nlarge/Desktop/test_nppes/NPPES_Data_Dissemination_February_2023.zip"
+    src_zip_file = download_source_file(source_url, source_file)
     source_file_list = []
     if src_zip_file:
         logging.info(f"Searching for source NPI data file within {src_zip_file}")
@@ -62,18 +63,15 @@ def main(
         for fileName in source_file_list:
             process_source_file(input_zip = src_zip_file,
                                 fileName = fileName,
-                                source_file = source_file,
-                                target_file = target_file,
                                 target_gcs_bucket = target_gcs_bucket,
                                 target_gcs_path = target_gcs_path,
                                 project_id = project_id,
                                 dataset_id = dataset_id,
                                 table_id = table_id,
                                 schema_path = schema_path,
-                                source_npi_data_file_regexp = source_npi_data_file_regexp,
                                 csv_headers = csv_headers,
-                                chunk_size = 1000
-                                )
+                                chunk_size = chunk_size
+            )
     else:
         logging.print("Failed: source zip file is invalid")
     logging.info(
@@ -84,111 +82,106 @@ def main(
 def process_source_file(
     input_zip: str,
     fileName: str,
-    source_file: pathlib.Path,
-    target_file: pathlib.Path,
     target_gcs_bucket: str,
     target_gcs_path: str,
     project_id: str,
     dataset_id: str,
     table_id: str,
     schema_path: str,
-    source_npi_data_file_regexp: str,
     csv_headers: typing.List[str],
     chunk_size: int,
 ) -> None:
-    logging.info(f"Found data file {fileName}, extracting ...")
-    # python_mem_obj = re.sub(r"([0-9]{2})\/([0-9]{2})\/([0-9]{4})", "\\3-\\1-\\2",
-    #                         extract_zip_to_mem(input_zip = src_zip_file,
-    #                                            filename_to_read = fileName)
-    #                                           )
-    # python_mem_obj = extract_zip_to_mem(input_zip = input_zip,
-    #                                     filename_to_read = fileName)
-    logging.info(f" ... Opening zipfile {input_zip}")
-    total_lines = 0
-    with ZipFile(input_zip) as zip_obj:
-        logging.info(f" ... Counting number of rows in compressed file {fileName}")
-        # total_lines = len(zip_obj.read(fileName).decode("UTF-8").splitlines(keepends=True))
-        total_lines = len(zip_obj.read(fileName).decode("UTF-8").splitlines(keepends=True))
-        logging.info(f"     ... Counted : {total_lines}")
-    lines_read = 0
-    batch_number = 1
-    batch_lines_read = 0
-    mem_obj = ""
-    with ZipFile(input_zip) as zip_obj:
-        logging.info(f" ... Reading compressed file {fileName}")
-        for line in zip_obj.read(fileName).decode("UTF-8").split("\n"):
-            # mem_obj = zip_obj.read(filename_to_read).split(b"\n")
-            if batch_lines_read == 0:
-                logging.info(f" ... Reading batch number {batch_number} compressed file {fileName}")
-            if lines_read != total_lines and (lines_read % chunk_size != 0):
-                logging.info(f" ... * Reading line {lines_read}")
-                mem_obj += f"{line}\n"
-            else:
-                logging.info(f" ... ** Reading line {lines_read}")
-                mem_obj += f"{line}"
-                with open(target_file, "w") as text_file:
-                    logging.info("Writing to target file")
-                    csv_header = ','.join(str(itm) for itm in csv_headers)
-                    text_file.write(f"{csv_header}\n")
-                with open(target_file, "a") as text_file:
-                    text_file.write(mem_obj)
-                    # text_file.write(python_mem_obj[python_mem_obj.find("\n") + 1:])
-                if os.path.exists(target_file):
-                    upload_file_to_gcs(
-                        file_path=target_file,
-                        target_gcs_bucket=target_gcs_bucket,
-                        target_gcs_path=f"{str.replace(target_gcs_path, '.csv', f'_{str(batch_number).zfill(3)}.csv')}",
-                    )
-                table_exists = create_dest_table(
-                    project_id=project_id,
-                    dataset_id=dataset_id,
-                    table_id=table_id,
-                    schema_filepath=schema_path,
-                    bucket_name=target_gcs_bucket,
-                    drop_table=False,
-                )
-                if table_exists:
-                    load_data_to_bq(
-                        project_id=project_id,
-                        dataset_id=dataset_id,
-                        table_id=table_id,
-                        file_path=target_file,
-                        truncate_table=True,
-                        field_delimiter=",",
-                    )
-                else:
-                    error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{destination_table} does not exist and/or could not be created."
-                    raise ValueError(error_msg)
-                batch_number += 1
-                batch_lines_read = 0
-                mem_obj = ""
-            lines_read += 1
+    logging.info(f"Found data file {fileName}, extracting and splitting ...")
+    zip_path = os.path.dirname(input_zip)
+    cmd = f"unzip -p {input_zip} {fileName} | tail -n +2 | split -l { str(chunk_size)} --additional-suffix '.csv' -d --filter='gzip -v9 > { zip_path }/$FILE.gz'"
+    subprocess.run(cmd, shell=True)
+    logging.info("Processing individual zip files ...")
+    for zip_file in sorted(pathlib.Path(zip_path).glob('x*.csv.gz')):
+        logging.info(f" ... File {zip_file}")
+        extracted_chunk = str(zip_file).replace(".gz", "")
+        gz_decompress(
+            infile=zip_file,
+            tofile=extracted_chunk,
+            delete_zipfile=True)
+        transform_data(
+            fileName = extracted_chunk,
+            csv_headers=csv_headers
+        )
+        load_source_file_to_bq(
+            target_file=extracted_chunk,
+            target_gcs_bucket=target_gcs_bucket,
+            target_gcs_path=target_gcs_path,
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            schema_path=schema_path,
+            truncate_table=(True if os.path.basename(extracted_chunk) == "x00.csv" else False),
+            field_delimiter=","
+        )
 
 
+def transform_data(
+    fileName: str,
+    csv_headers: typing.List[str]
+) -> None:
+    logging.info("Transforms ...")
+    logging.info(" ... Transform -> Adding header")
+    csv_header = ','.join(str(itm) for itm in csv_headers)
+    cmd = f"sed -i '1s/^/{csv_header}\\n/' {fileName}"
+    subprocess.run(cmd, shell=True)
+    logging.info(" ... Transform -> Resolving date format")
+    cmd = "sed -i -r -E 's/([0-9]{2})\/([0-9]{2})\/([0-9]{4})/\3-\1-\2/g;' " + f"{fileName}"
+    subprocess.run(cmd, shell=True)
+    logging.info("Transforms completed")
 
-def extract_zip_to_mem(input_zip: str, filename_to_read: str, bytes_batch_size_rows: int) -> str:
-    logging.info(f" ... Opening zipfile {input_zip}")
-    zip_obj = ZipFile(input_zip)
-    logging.info(f" ... Reading compressed file {filename_to_read}")
-    total_lines = zip_obj.read(filename_to_read).splitlines(keepends=True)
-    zip_obj.close()
-    lines_read = 0
-    bytes_batch_size_rows = 1000
-    zip_obj = ZipFile(input_zip)
-    for line in zip_obj.read(filename_to_read).split(b"\n"):
-        # mem_obj = zip_obj.read(filename_to_read).split(b"\n")
-        if lines_read != total_lines and (lines_read % bytes_batch_size_rows != 0):
-            mem_obj += f"{line}\n"
-        else:
-            mem_obj += f"{line}"
-            break
-    logging.info(" ... Closing zipfile")
-    zip_obj.close()
-    logging.info(" ... Decoding object to UTF-8")
-    mem_obj = mem_obj.decode("UTF-8")
-    logging.info(" ... Returning memory object")
-    return mem_obj
 
+def load_source_file_to_bq(
+    target_file: str,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+    project_id: str,
+    dataset_id: str,
+    table_id: str,
+    schema_path: str,
+    truncate_table: bool,
+    field_delimiter: str
+):
+    if os.path.exists(target_file):
+        upload_file_to_gcs(
+            file_path=target_file,
+            target_gcs_bucket=target_gcs_bucket,
+            target_gcs_path=f"{str.replace(target_gcs_path, '.csv', { os.path.basename(target_file) })}",
+        )
+    table_exists = create_dest_table(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        schema_filepath=schema_path,
+        bucket_name=target_gcs_bucket,
+        drop_table=False,
+    )
+    if table_exists:
+        load_data_to_bq(
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            file_path=target_file,
+            truncate_table=truncate_table,
+            field_delimiter=field_delimiter,
+        )
+        os.remove(target_file)
+    else:
+        error_msg = f"Error: Data was not loaded because the destination table {project_id}.{dataset_id}.{destination_table} does not exist and/or could not be created."
+        raise ValueError(error_msg)
+
+
+def gz_decompress(infile: str, tofile: str, delete_zipfile: bool = False) -> None:
+    logging.info(f"Decompressing {infile}")
+    with open(infile, "rb") as inf, open(tofile, "w", encoding="utf8") as tof:
+        decom_str = gzip.decompress(inf.read()).decode("utf-8")
+        tof.write(decom_str)
+    if delete_zipfile:
+        os.remove(infile)
 
 def download_source_file(source_url: str, source_file: str) -> str:
     logging.info(f"Downloading most recent source file")
@@ -367,5 +360,5 @@ if __name__ == "__main__":
         table_id=os.environ.get("TABLE_ID", ""),
         csv_headers=json.loads(os.environ.get("CSV_HEADERS", r"[]")),
         schema_path=os.environ.get("SCHEMA_PATH", ""),
-        chunk_size=os.environ.get("CHUNKSIZE", ""),
+        chunk_size=int(os.environ.get("CHUNKSIZE", "100000")),
     )
