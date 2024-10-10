4e4c1faccd6dd5d38b3c37843609dc66f584b6e3
Nicholas Large <84149918+nlarge-google@users.noreply.github.com>
2024-10-09 22:20:54 -0500

    Fix: Migrate bls 20241007 (#829)
    * fix: Resolved bad field data for field *deleted* in table full.
    
    * fix: Lint issues.
    
    * Fix: Migrate BLS Pipelines to new environment.
    
    * Fix: Migrate BLS Pipelines to new environment.
    
    * Fix: Post-testing fixes.

--- datasets/bls/pipelines/employment_hours_earnings_series/employment_hours_earnings_series_dag.py
+++ datasets/bls/pipelines/employment_hours_earnings_series/employment_hours_earnings_series_dag.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,13 +38,15 @@ with DAG(
         task_id="transform_csv",
         startup_timeout_seconds=600,
         name="employment_hours_earnings_series",
-        namespace="composer",
-        service_account_name="datasets",
+        namespace="composer-user-workloads",
+        service_account_name="default",
+        config_file="/home/airflow/composer_kube_config",
         image_pull_policy="Always",
         image="{{ var.json.bls.container_registry.run_csv_transform_kub }}",
         env_vars={
             "SOURCE_URLS": '["gs://pdp-feeds-staging/Bureau/ce.series.tsv"]',
             "SOURCE_FILES": '["files/data1.tsv"]',
+            "CHUNKSIZE": "50000",
             "TARGET_FILE": "files/data_output.csv",
             "TARGET_GCS_BUCKET": "{{ var.value.composer_bucket }}",
             "TARGET_GCS_PATH": "data/bls/employment_hours_earnings_series/data_output.csv",
@@ -53,10 +55,10 @@ with DAG(
             "TRIM_SPACE": '["series_id","footnote_codes"]',
             "CSV_HEADERS": '["series_id","supersector_code","industry_code","data_type_code","seasonal","series_title","footnote_codes","begin_year","begin_period","end_year","end_period"]',
         },
-        resources={
-            "request_memory": "4G",
-            "request_cpu": "1",
-            "request_ephemeral_storage": "10G",
+        container_resources={
+            "memory": {"request": "8Gi"},
+            "cpu": {"request": "1"},
+            "ephemeral-storage": {"request": "10Gi"},
         },
     )
 
@@ -64,7 +66,7 @@ with DAG(
     load_to_bq = gcs_to_bigquery.GCSToBigQueryOperator(
         task_id="load_to_bq",
         bucket="{{ var.value.composer_bucket }}",
-        source_objects=["data/bls/employment_hours_earnings_series/data_output.csv"],
+        source_objects=["data/bls/employment_hours_earnings_series/data_output-*.csv"],
         source_format="CSV",
         destination_project_dataset_table="bls.employment_hours_earnings_series",
         skip_leading_rows=1,
