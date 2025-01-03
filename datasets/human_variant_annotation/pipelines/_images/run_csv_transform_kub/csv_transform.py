040cda5f93a4295da6618f08fa4f33dfe6e1f5e9
afalam359 <afalam@google.com>
2025-01-03 20:31:15 +0530

    Fix: Code changes for human_variant_annotation DAG to fix value error (#888)
    * Fix: Code changes for human_variant_annotation DAG to fix value error
    
    * Fix: Comment removed from code to fix lint failure

--- datasets/human_variant_annotation/pipelines/_images/run_csv_transform_kub/csv_transform.py
+++ datasets/human_variant_annotation/pipelines/_images/run_csv_transform_kub/csv_transform.py
@@ -49,7 +49,10 @@ def main(
 
 def get_dates() -> typing.List[str]:
     today = datetime.datetime.now()
-    start_date = datetime.datetime(today.year, today.month - 1, 1)
+    if today.month == 1:
+        start_date = datetime.datetime(today.year - 1, 12, 1)
+    else:
+        start_date = datetime.datetime(today.year, today.month - 1, 1)
     end_date = today
     delta = datetime.timedelta(days=1)
     dates = []
